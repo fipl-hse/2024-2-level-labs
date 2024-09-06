9b94f52feab4846bb9bade4221fdd8469faf79f5
Belofon <sosghset@gmail.com>
2024-09-06 18:08:23 +0300

    Change 'Return' from NONE to STR

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,8 +3,8 @@ Lab 1.
 """
 
 
-def say_hello_world() -> None:
-    """
-    Say hello world.
-    """
-    return None
+def say_hello_world(): #Remove 'None' setting, return STR
+    return 'Hello world'
+
+
+print(say_hello_world())

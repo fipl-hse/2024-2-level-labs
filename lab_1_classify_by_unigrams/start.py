dc170e0dc0bfa6d5ecf1af3c74c8024eb2383584
ShrubberyBush <natablack58@gmail.com>
2024-09-17 22:10:14 +0300

    Labs

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -18,6 +18,8 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
+    result = tokens
+    assert result, "Detection result is None"
 
 
 if __name__ == "__main__":

dd61a9e58cc89a145dbd4a476fb0d1c348f6b2d7
Helenhelen2005 <elchevtaeva@gmail.com>
2024-09-21 18:08:11 +0300

    fixing..

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,13 +1,9 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import detect_language
-
-from lab_1_classify_by_unigrams.main import create_language_profile
-
-from lab_1_classify_by_unigrams.main import tokenize
-
 # pylint:disable=too-many-locals, unused-argument, unused-variable
+
+from lab_1_classify_by_unigrams.main import detect_language, create_language_profile, tokenize
 
 
 def main() -> None:
@@ -16,15 +12,15 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        tokens = en_text
+        tokens = tokenize(en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
-    print(tokenize(en_text))
     print(create_language_profile("eng", en_text))
-    print(detect_language(create_language_profile("unknown", unknown_text), create_language_profile("en", en_text), create_language_profile("de", de_text)))
+    print(detect_language(create_language_profile("unknown", unknown_text), create_language_profile("en", en_text),
+                          create_language_profile("de", de_text)))
     result = tokens
 
     assert result, "Detection result is None"

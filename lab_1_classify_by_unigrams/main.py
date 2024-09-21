ddec5059605f6151fa50c76116addcee3cab19a5
semtassta <semtassta@gmail.com>
2024-09-21 13:12:54 +0300

    mark 8 adjustments for comments

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -224,7 +224,7 @@ def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, 
 
 
 def detect_language_advanced(
-        unknown_profile: dict[str, str | dict[str, float]], known_profiles: list
+    unknown_profile: dict[str, str | dict[str, float]], known_profiles: list
 ) -> list | None:
     """
     Detect the language of an unknown profile.
--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -19,7 +19,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = 1  # None
+    # result = 1  # None
     print(tokenize(en_text))
     print(create_language_profile('en', en_text))
     profile_paths = [
@@ -31,7 +31,7 @@ def main() -> None:
         "assets/profiles/ru.json",
         "assets/profiles/tr.json"
     ]
-    assert result, "Detection result is None"
+    # assert result, "Detection result is None"
 
 
 if __name__ == "__main__":

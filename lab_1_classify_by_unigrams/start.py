ebfbe1e49e8fbba4a9090a7824d9813460a8d4ae
MariaNikitenko19 <marianikitenko19@gmail.com>
2024-09-22 12:02:47 +0300

    create language profile

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,8 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import (tokenize, calculate_frequencies, create_language_profile)
+from lab_1_classify_by_unigrams.main import (create_language_profile)
+
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
 
@@ -11,15 +12,15 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        #tokenize(en_text)
-        print(create_language_profile('english',en_text))
+        # tokenize(en_text)
+        print(create_language_profile('english', en_text))
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        tokenize(de_text)
+        # tokenize(de_text)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-        tokenize(unknown_text)
-    result = tokenize
+        # tokenize(unknown_text)
+    result = create_language_profile
     assert result, "Detection result is None"
 
 

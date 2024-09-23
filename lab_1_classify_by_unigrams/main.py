31ffef1fabbd53bb1b0d26bf0add7a44d7b5053e
ShrubberyBush <natablack58@gmail.com>
2024-09-23 23:12:04 +0300

    code style fixes

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -178,8 +178,7 @@ def detect_language(
                 return profile_1.get('name')
             if mse_prof_1 > mse_prof_2:
                 return profile_2.get('name')
-            else:
-                return min(profile_1.get('name'), profile_2.get('name'))
+            return min(profile_1.get('name'), profile_2.get('name'))
 
 
 def load_profile(path_to_file: str) -> dict | None:

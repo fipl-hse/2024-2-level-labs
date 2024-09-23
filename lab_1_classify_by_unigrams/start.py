1808fe978dc822e0bd2c1d0cee77437dbec67d30
Helenhelen2005 <elchevtaeva@gmail.com>
2024-09-23 22:07:30 +0300

    new

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -13,15 +13,18 @@ def main() -> None:
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
         tokens = tokenize(en_text)
+        en_profile = create_language_profile("eng", en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
+        de_profile = create_language_profile("de", de_text)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
+        unk_profile = create_language_profile("unknown", unknown_text)
 
     print(create_language_profile("eng", en_text))
-    print(detect_language(create_language_profile("unknown", unknown_text),
-                          create_language_profile("en", en_text),
-                          create_language_profile("de", de_text)))
+    if en_profile is not None and de_profile is not None and unk_profile is not None:
+        print(detect_language(en_profile, de_profile, unk_profile))
+
     result = tokens
 
     assert result, "Detection result is None"

1d95386dd773d26910d4d02b88a3a888d4d18bcc
melkern <melkonyankarina26@gmail.com>
2024-09-26 23:49:35 +0300

    hope it will work

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -3,8 +3,7 @@ Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 from lab_1_classify_by_unigrams.main import (create_language_profile,
-                                             detect_language, tokenize,
-                                             compare_profiles)
+                                             detect_language, tokenize)
 
 
 def main() -> None:
@@ -13,7 +12,7 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        tokenize(en_text)
+        tokens = tokenize(en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
@@ -23,11 +22,9 @@ def main() -> None:
     de_profile = create_language_profile("de", de_text)
     unknown_profile = create_language_profile('unknown', unknown_text)
 
-    compare_profiles(unknown_profile, en_profile)
-
-    if unknown_profile is None or en_profile is None or de_profile is None:
-        return None
-    result = detect_language(unknown_profile, en_profile, de_profile)
+    if unknown_profile is not None or en_profile is not None or de_profile is not None:
+        print(detect_language(unknown_profile, en_profile, de_profile))
+    result = tokens
     assert result, "Detection result is None"
 
 

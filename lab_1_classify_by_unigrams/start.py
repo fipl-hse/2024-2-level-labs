a44ff335f576819f0a6aedf98304995fb683b28b
evgraff2005 <annaevgrafova22095@gmail.com>
2024-09-27 20:10:53 +0300

    ...

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -23,10 +23,10 @@ def main() -> None:
         unknown_text = file_to_read_unk.read()
         unknown_profile = create_language_profile('unknown', unknown_text)
 
-    if (not isinstance(unknown_profile, dict)
+    '''if (not isinstance(unknown_profile, dict)
             or not isinstance(profile_to_compare_en, dict)
             or not isinstance(profile_to_compare_de, dict)):
-        return None
+        return None'''
     answer = detect_language(unknown_profile, profile_to_compare_en, profile_to_compare_de)
 
     result = answer

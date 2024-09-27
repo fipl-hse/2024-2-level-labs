1be801465bda049b0ad5b37eb31fab3f00015c52
Ksenia Maiorova <mayorova.kseniya.05@mail.ru>
2024-09-27 13:40:59 +0300

    update

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
-from lab_1_classify_by_unigrams.main import (tokenize, create_language_profile)
+from lab_1_classify_by_unigrams.main import create_language_profile, tokenize
 
 
 def main() -> None:
@@ -15,7 +15,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
+    result = en_text
     assert result, "Detection result is None"
     print(tokenize(en_text))
     print(create_language_profile("en", en_text))

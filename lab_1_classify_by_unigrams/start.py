6a084b2161638ea759f0d46b04b48bfe5ec3e057
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-09-28 17:33:59 +0300

    Hotfix 2

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -10,11 +10,11 @@ def main() -> None:
     """
     Launches an implementation
     """
-    with open("lab_1_classify_by_unigrams/assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-    with open("lab_1_classify_by_unigrams/assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-    with open("lab_1_classify_by_unigrams/assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
     unknown_profile = funcs.create_language_profile("unk", unknown_text)

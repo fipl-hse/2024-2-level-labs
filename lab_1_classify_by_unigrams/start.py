f8d59528982e3c99fdd12dc22b8591f6c318bc34
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-09-28 17:31:27 +0300

    Test: added a check in start.py

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -10,18 +10,22 @@ def main() -> None:
     """
     Launches an implementation
     """
-    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+    with open("lab_1_classify_by_unigrams/assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+    with open("lab_1_classify_by_unigrams/assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+    with open("lab_1_classify_by_unigrams/assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
     unknown_profile = funcs.create_language_profile("unk", unknown_text)
     profile_1 = funcs.create_language_profile("en", en_text)
     profile_2 = funcs.create_language_profile("de", de_text)
 
-    result = funcs.detect_language(unknown_profile, profile_1, profile_2)
+    if not all(isinstance(prof, dict)
+               for prof in (unknown_profile, profile_1, profile_2)):
+        result = None
+    else:
+        result = funcs.detect_language(unknown_profile, profile_1, profile_2)
     assert result, "Detection result is None"
     print(result)
 

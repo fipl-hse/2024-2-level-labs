a9b835d079d4bf9adab16e5291fd293689747af7
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-09-28 17:51:12 +0300

    Tweaked start.py

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -17,15 +17,11 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
-    unknown_profile = funcs.create_language_profile("unk", unknown_text)
-    profile_1 = funcs.create_language_profile("en", en_text)
-    profile_2 = funcs.create_language_profile("de", de_text)
+    unknown_profile: dict = funcs.create_language_profile("unk", unknown_text)
+    profile_1: dict = funcs.create_language_profile("en", en_text)
+    profile_2: dict = funcs.create_language_profile("de", de_text)
 
-    if not all(isinstance(prof, dict)
-               for prof in (unknown_profile, profile_1, profile_2)):
-        result = None
-    else:
-        result = funcs.detect_language(unknown_profile, profile_1, profile_2)
+    result = funcs.detect_language(unknown_profile, profile_1, profile_2)
     assert result, "Detection result is None"
     print(result)
 

5b4fd43455f451cce7c2e73833977c4adb619812
semtassta <semtassta@gmail.com>
2024-09-28 11:08:57 +0300

    make it green!

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import tokenize, create_language_profile
+from lab_1_classify_by_unigrams.main import create_language_profile, tokenize
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 

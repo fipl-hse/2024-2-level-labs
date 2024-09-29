c9e78dc3383eec4459e8e0c02b6143642b3520d1
seliverstovaa <asya030205@gmail.com>
2024-09-29 18:11:44 +0300

    fixed comments

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -21,14 +21,14 @@ def tokenize(text: str) -> list[str] | None:
 
     In case of corrupt input arguments, None is returned
     """
-    if isinstance(text, str):
-        prepared_text = text.lower()
-        tokens = []
-        for i in prepared_text:
-            if i.isalpha():
-                tokens.append(i)
-        return tokens
-    return None
+    if not isinstance(text, str):
+        return None
+    prepared_text = text.lower()
+    tokens = []
+    for tok in prepared_text:
+        if tok.isalpha():
+            tokens.append(tok)
+    return tokens
 
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
@@ -43,18 +43,20 @@ def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
 
                 In case of corrupt input arguments, None is returned
                 """
-    if isinstance(tokens, list):
-        if isinstance(tokens[0], str):
-            frequency = {}
-            tokens_number = len(tokens)
-            for letter in tokens:
-                if isinstance(letter, str):
-                    letter_number = tokens.count(letter)
-                    letter_index = letter_number/tokens_number
-                    letter_dict = {letter: letter_index}
-                    frequency.update(letter_dict)
-            return frequency
-    return None
+    if not isinstance(tokens, list):
+        return None
+    if not isinstance(tokens[0], str):
+        return None
+    frequency = {}
+    tokens_number = len(tokens)
+    for letter in tokens:
+        if isinstance(letter, str):
+            return None
+        letter_number = tokens.count(letter)
+        letter_index = letter_number/tokens_number
+        letter_dict = {letter: letter_index}
+        frequency.update(letter_dict)
+    return frequency
 
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
@@ -78,7 +80,6 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     return {"name": language, "freq": freq_dict}
 
 
-
 def calculate_mse(predicted: list, actual: list) -> float | None:
     """
         Calculate mean squared error between predicted and actual values.
@@ -92,18 +93,18 @@ def calculate_mse(predicted: list, actual: list) -> float | None:
 
         In case of corrupt input arguments, None is returned
         """
-    if isinstance(predicted, list) and isinstance(actual, list):
-        if len(actual) == len(predicted):
-            summa = 0.0
-            n = len(actual)
-            for i in range(0, n):
-                a = float(actual[i])
-                p = float(predicted[i])
-                sqw_dif = float((a - p)**2)
-                summa = float(summa + sqw_dif)
-            mse = summa/n
-            return mse
-    return None
+    if not isinstance(predicted, list) or not isinstance(actual, list):
+        return None
+    if len(actual) != len(predicted):
+        return None
+    summa = 0.0
+    for ind in range(0, len(actual)):
+        a = float(actual[ind])
+        p = float(predicted[ind])
+        sqw_dif = float((a - p)**2)
+        summa = float(summa + sqw_dif)
+    mse = summa/len(actual)
+    return mse
 
 
 def compare_profiles(unknown_profile: dict[str, str | dict[str, float]],
@@ -122,38 +123,40 @@ def compare_profiles(unknown_profile: dict[str, str | dict[str, float]],
         In case of corrupt input arguments or lack of keys 'name' and
         'freq' in arguments, None is returned
         """
-    if isinstance(unknown_profile, dict) and isinstance(profile_to_compare, dict):
-        if len(unknown_profile) == 2 and len(profile_to_compare) == 2:
-            unknown_freq = unknown_profile.get("freq").copy()
-            compare_freq = profile_to_compare.get("freq").copy()
-            for tok in unknown_freq:
-                if tok not in compare_freq:
-                    new_tok = {tok: 0.0}
-                    compare_freq.update(new_tok)
-            compare_freq_l = sorted(compare_freq.items())
-            compare_freq_new = {}
-            for elem in compare_freq_l:
-                if len(elem) == 2:
-                    token_for_new = {elem[0]: elem[1]}
-                    compare_freq_new.update(token_for_new)
+    if not isinstance(unknown_profile, dict) or not isinstance(profile_to_compare, dict):
+        return None
+    if len(unknown_profile) != 2 and len(profile_to_compare) != 2:
+        return None
+    unknown_freq = unknown_profile.get("freq").copy()
+    compare_freq = profile_to_compare.get("freq").copy()
+    for tok in unknown_freq:
+        if tok not in compare_freq:
+            new_tok = {tok: 0.0}
+            compare_freq.update(new_tok)
+    compare_freq_l = sorted(compare_freq.items())
+    compare_freq_new = {}
+    for elem in compare_freq_l:
+        if len(elem) == 2:
+            token_for_new = {elem[0]: elem[1]}
+            compare_freq_new.update(token_for_new)
+    for tok in compare_freq:
+        if tok not in unknown_freq:
+            new_tok = {tok: 0.0}
+            unknown_freq.update(new_tok)
+    unknown_freq_l = sorted(unknown_freq.items())
+    unknown_freq_new = {}
+    for elem in unknown_freq_l:
+        if len(elem) == 2:
+            token_for_new = {elem[0]: elem[1]}
+            unknown_freq_new.update(token_for_new)
+    profile_pr = list(compare_freq_new.values())
+    profile_act = list(unknown_freq_new.values())
+    mse = calculate_mse(profile_pr, profile_act)
+    if not isinstance(mse, float):
+        return None
+    mse = round(mse, 4)
+    return mse
 
-            for tok in compare_freq:
-                if tok not in unknown_freq:
-                    new_tok = {tok: 0.0}
-                    unknown_freq.update(new_tok)
-            unknown_freq_l = sorted(unknown_freq.items())
-            unknown_freq_new = {}
-            for elem in unknown_freq_l:
-                if len(elem) == 2:
-                    token_for_new = {elem[0]: elem[1]}
-                    unknown_freq_new.update(token_for_new)
-            profile_pr = list(compare_freq_new.values())
-            profile_act = list(unknown_freq_new.values())
-            mse = calculate_mse(profile_pr, profile_act)
-            if isinstance(mse, float):
-                mse = round(mse, 4)
-            return mse
-    return None
 
 
 def detect_language(
@@ -217,12 +220,12 @@ def load_profile(path_to_file: str) -> dict | None:
     In case of corrupt input arguments, None is returned
     """
 
-    if isinstance(path_to_file, str):
-        with open(path_to_file, "r", encoding="utf-8") as file_to_read:
-            profile_r = json.load(file_to_read)
-            profile = dict(profile_r)
-        return profile
-    return None
+    if not isinstance(path_to_file, str):
+        return None
+    with open(path_to_file, "r", encoding="utf-8") as file_to_read:
+        profile_r = json.load(file_to_read)
+        profile = dict(profile_r)
+    return profile
 
 
 def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
@@ -244,7 +247,6 @@ def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
     frequency = profile.get("freq")
     if not frequency:
         return None
-    new_profile = {}
     name = profile.get("name")
     if not isinstance(name, str):
         return None
@@ -257,17 +259,18 @@ def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
         return None
     n_words = n_words_list[0]
     for elem in tokens:
-        if isinstance(elem, str) and len(elem) == 1:
-            number = frequency.get(elem)
-            let = elem.lower()
-            if isinstance(let, str) and let not in letters:
-                new_token = {let: number}
-                letters.update(new_token)
-            else:
-                freq_add = letters.get(let)
-                sum_freq = number + freq_add
-                new_token = {let: sum_freq}
-                letters.update(new_token)
+        if not isinstance(elem, str) or len(elem) != 1:
+            return None
+        number = frequency.get(elem)
+        let = elem.lower()
+        if isinstance(let, str) and let not in letters:
+            new_token = {let: number}
+            letters.update(new_token)
+        else:
+            freq_add = letters.get(let)
+            sum_freq = number + freq_add
+            new_token = {let: sum_freq}
+            letters.update(new_token)
     for tok in letters:
         freq_index = float(letters.get(tok)/n_words)
         letters[tok] = freq_index

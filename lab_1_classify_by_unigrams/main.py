95399c7b3e2416514b6926bafbc03c10f29b95cd
AlinaBatrasova <chasogeyka@gmail.com>
2024-09-29 22:47:57 +0300

    Finished 1-4, struggling with 5 and need help TT

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -5,6 +5,9 @@ Language detection
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
+def clever_round(n, decimals=0):
+    multiplier = 10**decimals
+    return int(n * multiplier) / multiplier
 
 def tokenize(text: str) -> list[str] | None:
     """
@@ -20,7 +23,20 @@ def tokenize(text: str) -> list[str] | None:
 
     In case of corrupt input arguments, None is returned
     """
+    if type(text) != str:
+        return None
 
+    text = text.lower()
+    num = '1234567890'
+    symb = ',./?!:;#@*-&<>%'
+    tokens = []
+    for elem in text:
+        if elem not in num and elem not in symb and elem != ' ':
+            tokens.append(elem)
+    return tokens
+
+text = 'Hey! How are you?'
+print(tokenize(text))
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
     """
@@ -34,7 +50,27 @@ def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
 
     In case of corrupt input arguments, None is returned
     """
+    token_freq = {}
+    if not isinstance(tokens, list):
+        return None
 
+    if not tokens:
+        return None
+
+    for letter in tokens:
+        if not isinstance(letter, str) or len(letter) > 1:
+            return None
+        if letter not in token_freq:
+            token_freq[letter] = 1
+        else:
+            token_freq[letter] += 1
+
+    for key, value in token_freq.items():
+        token_freq[key] = clever_round(value/len(tokens),4)
+    return token_freq
+
+#bad_inputs = ['string', {}, (), None, 9, 9.34, True, [None]]
+#print(calculate_frequencies(bad_inputs))
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
     """
@@ -49,8 +85,16 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
 
     In case of corrupt input arguments, None is returned
     """
+    if not tokenize(text):
+        return None
+    if not isinstance(language, str):
+        return None
+    language_profile = {}
+    language_profile['name'] = language
+    language_profile['freq'] = calculate_frequencies(tokenize(text))
+    return language_profile
 
-
+print(create_language_profile('en', text))
 def calculate_mse(predicted: list, actual: list) -> float | None:
     """
     Calculate mean squared error between predicted and actual values.
@@ -64,7 +108,15 @@ def calculate_mse(predicted: list, actual: list) -> float | None:
 
     In case of corrupt input arguments, None is returned
     """
+    mse_sum = 0
 
+    if len(predicted) != len(actual):
+        return None
+
+    for i in range(len(predicted)):
+        mse_sum += (predicted[i] - actual[i]) ** 2
+
+    return clever_round(mse_sum / len(predicted),4)
 
 def compare_profiles(
     unknown_profile: dict[str, str | dict[str, float]],
@@ -84,7 +136,24 @@ def compare_profiles(
     In case of corrupt input arguments or lack of keys 'name' and
     'freq' in arguments, None is returned
     """
+    shared_tokens = []
+    if len(profile_to_compare) != len(unknown_profile):
+        return None
+    for elem_known in profile_to_compare:
+        for elem_who_knows in unknown_profile:
+            if elem_known == elem_who_knows:
+                shared_tokens.append(elem_known)
 
+#aaaaa я вообще не понимаю как работает compare_profiles даже теоретически....
+'''В данных профилях встречаются следующие символы: ``a``, ``b``, ``c``.
+При этом в профиле первого языка их встречаемость равна
+``[0.5, 0.5, 0]``, а в профиле второго языка - ``[0, 0.5, 0.5]``.
+
+Приняв встречаемость символов в первом языке за истинные значения и
+встречаемость символов во втором языке за предсказанные, мы можем
+рассчитать разницу профилей по метрике ``MSE``. Ее значение будет равно
+``0.167`` (с округлением до третьего знака).'''
+'''Вот откуда берется 0.167 я не понимаю, поэтому как считать тоже :( Пробовала общаться с гпт, но не помогло, помогите пожалуйста((((((('''
 
 def detect_language(
     unknown_profile: dict[str, str | dict[str, float]],

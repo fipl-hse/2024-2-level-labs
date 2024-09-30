d8afcd37996ed8e96962ba0366ee55e633024a61
LexanDar11o6 <lexandar11o6@gmail.com@>
2024-10-01 01:08:05 +0300

    6

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -5,7 +5,21 @@ Language detection
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
-def tokenize(text):
+
+def tokenize(text: str) -> list[str] | None:
+    """
+    Split a text into tokens.
+
+    Convert the tokens into lowercase, remove punctuation, digits and other symbols
+
+    Args:
+        text (str): A text
+
+    Returns:
+        list[str] | None: A list of lower-cased tokens without punctuation
+
+    In case of corrupt input arguments, None is returned
+    """
 
     if type(text) is not str:
         return None
@@ -17,17 +31,116 @@ def tokenize(text):
             token_list.append(symbol)
     return token_list
 
-def calculate_frequencies(token_text):
+def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
+    """
+    Calculate frequencies of given tokens.
+
+    Args:
+        tokens (list[str] | None): A list of tokens
+
+    Returns:
+        dict[str, float] | None: A dictionary with frequencies
+
+    In case of corrupt input arguments, None is returned
+    """
+
+    if type(tokens) is not list[str]:
+        return None
 
     freq_dic = {}
-    for letter in token_text:
-        freq_dic[letter] = token_text.count(letter) / len(token_text)
+    for letter in tokens:
+        freq_dic[letter] = tokens.count(letter) / len(tokens)
     return freq_dic
 
-def create_language_profile(language,text):
+def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
+    """
+    Create a language profile.
 
-    language_profile = {"name":language,"freq":calculate_frequencies(tokenize(text))}
+    Args:
+        language (str): A language
+        text (str): A text
+
+    Returns:
+        dict[str, str | dict[str, float]] | None: A dictionary with two keys – name, freq
+
+    In case of corrupt input arguments, None is returned
+    """
+
+    if type(text) is not str:
+        return None
+    if type(language) is not str:
+        return None
+
+    freq = calculate_frequencies(tokenize(text))
+
+    language_profile = {"name":language,"freq":freq}
     return language_profile
+
+
+def calculate_mse(predicted: list, actual: list) -> float | None:
+    """
+    Calculate mean squared error between predicted and actual values.
+
+    Args:
+        predicted (list): A list of predicted values
+        actual (list): A list of actual values
+
+    Returns:
+        float | None: The score
+
+    In case of corrupt input arguments, None is returned
+    """
+
+
+
+
+def compare_profiles(
+    unknown_profile: dict[str, str | dict[str, float]],
+    profile_to_compare: dict[str, str | dict[str, float]],
+) -> float | None:
+    """
+    Compare profiles and calculate the distance using symbols.
+
+    Args:
+        unknown_profile (dict[str, str | dict[str, float]]): A dictionary of an unknown profile
+        profile_to_compare (dict[str, str | dict[str, float]]): A dictionary of a profile
+            to compare the unknown profile to
+
+    Returns:
+        float | None: The distance between the profiles
+
+    In case of corrupt input arguments or lack of keys 'name' and
+    'freq' in arguments, None is returned
+    """
+
+
+def detect_language(
+    unknown_profile: dict[str, str | dict[str, float]],
+    profile_1: dict[str, str | dict[str, float]],
+    profile_2: dict[str, str | dict[str, float]],
+) -> str | None:
+    """
+    Detect the language of an unknown profile.
+
+    Args:
+        unknown_profile (dict[str, str | dict[str, float]]): A dictionary of a profile
+            to determine the language of
+        profile_1 (dict[str, str | dict[str, float]]): A dictionary of a known profile
+        profile_2 (dict[str, str | dict[str, float]]): A dictionary of a known profile
+
+    Returns:
+        str | None: A language
+
+    In case of corrupt input arguments, None is returned
+    """
+
+
+
+
+
+
+
+
 
 # def calculate_mse(predicted, actual):
 #

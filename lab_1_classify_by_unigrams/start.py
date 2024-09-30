b1ce822224832f26d6313721a09c612c35bedd93
AlinaBatrasova <chasogeyka@gmail.com>
2024-09-30 22:17:38 +0300

    aaaa

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
-from main import create_language_profile
+import main
 
 def main() -> None:
     """
@@ -14,7 +14,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = create_language_profile('enflish', en_text)
+    result = main.create_language_profile('enflish', en_text)
     assert result, "Detection result is None"
 
 

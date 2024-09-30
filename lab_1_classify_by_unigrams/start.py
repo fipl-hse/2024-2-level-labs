292e0a83f95fc4a7a22b4c3be8e4fc451ab298e5
kotelnikovalizo4ka <kotelnikovalizo4ka>
2024-09-30 17:20:05 +0300

    message

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -3,24 +3,25 @@ Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
-from main import tokenize
-from main import calculate_frequencies
 from main import create_language_profile
 
 def main() -> None:
 
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        profile = create_language_profile('en',en_text)
+        profile = create_language_profile('en', en_text)
         print(profile)
+
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        profile = create_language_profile('de',de_text)
+        profile = create_language_profile('de', de_text)
         print(profile)
+
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-        profile = create_language_profile('unknown',unknown_text)
+        profile = create_language_profile('unknown', unknown_text)
         print(profile)
+
     result = profile
     assert result, "Detection result is None"
 

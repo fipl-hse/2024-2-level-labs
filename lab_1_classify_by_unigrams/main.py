2c660f75abfc65470bd153a7589b28f8ba4fd105
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-09-30 21:26:57 +0300

    Turned append(0) into append(0.0) in COMPARE_PROFILES to see if it gives any effect

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -139,12 +139,12 @@ def compare_profiles(
     freq_list_to_compare = []
     for key in all_keys:
         if not key in unknown_profile["freq"]:
-            unknown_freq_list.append(0)
+            unknown_freq_list.append(0.0)
         else:
             freq_of_key = unknown_profile["freq"][key]
             unknown_freq_list.append(freq_of_key)
         if not key in profile_to_compare["freq"]:
-            freq_list_to_compare.append(0)
+            freq_list_to_compare.append(0.0)
         else:
             freq_of_key = profile_to_compare["freq"][key]
             freq_list_to_compare.append(freq_of_key)

868d86251399961644742528a1489796afe53eb0
kotelnikovalizo4ka <kotelnikovalizo4ka>
2024-09-30 17:40:27 +0300

    message

--- seminars/practice_3_lists.py
+++ seminars/practice_3_lists.py
@@ -145,7 +145,7 @@ def create_phone_number(nums: list) -> str:
     Write a function that accepts an array of 10 integers (between 0 and 9),
     that returns a string of those numbers in the form of a phone number.
     """
-    freturn '({nums[0:3]}) '
+    return '({nums[0:3]}) '
 
 # Function calls with expected result:
 # create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])

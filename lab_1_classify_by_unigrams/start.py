9baeeb66a5ddac6c05af76a80201724eb68abfd1
AlinaBatrasova <chasogeyka@gmail.com>
2024-09-30 22:04:05 +0300

    i did start.py! but i do not want 6, i will work more for 8

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,6 +2,7 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
+import main
 
 def main() -> None:
     """
@@ -13,7 +14,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
+    result = print(main.create_language_profile('enflish', en_text))
     assert result, "Detection result is None"
 
 

da9627387b9ec26b826daa67bf5d68f42b826d92
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-09-30 22:47:06 +0300

    start

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -10,12 +10,11 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        m.create_language_profile('en', en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
+    result = print(m.create_language_profile('en', en_text))
     assert result, "Detection result is None"
 
 

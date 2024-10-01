3768b6a72c4fd26e69b87774be57ee8834f1cdda
QuietHellsPage <baazhinga@yandex.ru>
2024-10-01 22:50:59 +0300

    55

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,10 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
-from lab_1_classify_by_unigrams.main import (create_language_profile, detect_language)
+from lab_1_classify_by_unigrams.main import (
+    create_language_profile,
+    detect_language
+)
 
 def main() -> None:
     """

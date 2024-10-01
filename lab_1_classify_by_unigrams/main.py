50bc4d8c64a43f36afe603c1cbfccb97ad2304ca
zakievamaria <zakievama@gmail.com>
2024-10-01 22:00:13 +0300

    mypy#6

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -253,9 +253,8 @@ def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, 
     for path in paths_to_profiles:
         profile_raw = load_profile(path)
         processed_profile = preprocess_profile(profile_raw)
-        if (not all(isinstance(k, str) for k in processed_profile.keys())
-                or not all(isinstance(k, str) for k in processed_profile['freq'].keys())
-                or not all(isinstance(v, float) for v in processed_profile['freq'].values())):
+        if (not all(isinstance((k, v), str) for k, v in processed_profile.items()) or not
+        all(isinstance((k, str), (v, float)) for k, v in processed_profile['freq'].items())):
             return None
         all_profiles.append(processed_profile)
     return all_profiles

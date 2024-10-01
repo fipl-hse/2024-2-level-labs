8b0443d7f3126148e5e3afe8e5150441ade657d7
Анна <anna@Annas-Air.IGD_Rostelecom>
2024-10-02 01:47:08 +0300

    changes in start

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -5,11 +5,7 @@ Language detection starter
 
 # pylint:disable=too-many-locals , unused-argument, unused-variable
 
-from main import tokenize
-
-from main import calculate_frequencies
-
-from main import create_language_profile
+from main import tokenize, calculate_frequencies, create_language_profile
 
 def main() -> None:
     """

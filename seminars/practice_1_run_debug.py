9d6954e89d09665c252238556d55461cad24fa13
MariaManukhina <masamanukhina@gmail.com>
2024-10-02 18:57:46 +0300

    delete pracrise

--- seminars/practice_1_run_debug.py
+++ seminars/practice_1_run_debug.py
@@ -1,12 +1,3 @@
-a = 0
-b = a
-c = 0
-print(b, id(b), id(c))
-print(a, id(a))
-a = a + 1
-print(a, b, id(b), id(a))
-a += 1
-print(a, id(a))
 
 
 
@@ -29,34 +20,3 @@ Running python application and debugging
 # 3. Debugging is the first skill of a programmer.
 #       It helps you to take a step-by-step look at how the program works.
 
-
-# Debugging exercise. Debug the program and fix errors:
-first_num = 4
-second_num = 3
-#print(f'Numbers: {first_num} and {second_num}')
-
-# Case1: the first exception trigger:
-# third_num = first_num / second_num
-
-# Fix the first exception:
-second_num = 3
-third_num = first_num / second_num
-#print(third_num)
-#print(f'Numbers: {first_num}, {second_num} and {third_num}')
-c = 1.3333333
-stroka = str(c)
-#print(f(stroka))
-
-a = f"Моё число: {c:.2f}" #сначала в скобках число, если мы хотим отформатировать то {.format(c)} f говорит о float, .2 значит что после точки 2 цифры
-print(a)
-exit(0)
-#типы данных, по которым можно интегрироваться, называются последовательностями
-#при умножении на флот сравниваются типы данных , при уможении на отрицательное, но целое число проверки проходят
-
-# Case 2: the second debugging case:
-if first_num * second_num == third_num:
-    print('First number * second equals third number')
-else:
-    print('First number * second does not equal third number')
-
-print('Program finished')

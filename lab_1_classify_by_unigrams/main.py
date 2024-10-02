96e63d5bb3c4078f141f9fddc0a9a3f11000382c
Морева Дарья <doshirak14@inbox.ru>
2024-10-03 00:19:11 +0300

    finish

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -182,6 +182,7 @@ def detect_language(
         return None
     mse_1 = compare_profiles(unknown_profile, profile_1)
     mse_2 = compare_profiles(unknown_profile, profile_2)
+    profiles_names: str = sorted([profile_1["name"], profile_2["name"]])[0]
     if (not isinstance(mse_1, float)
             or not isinstance(mse_2,  float)):
         return None
@@ -189,9 +190,7 @@ def detect_language(
         return str(profile_1["name"])
     if mse_1 > mse_2:
         return str(profile_2["name"])
-    if mse_1 == mse_2:
-        return sorted([profile_1["name"], profile_2["name"]])[0]
-    return None
+    return profiles_names
 
 
 def load_profile(path_to_file: str) -> dict | None:

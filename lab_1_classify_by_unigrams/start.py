95939e6770e7fc0387e3073c7e6a490f560dba3b
ZlataMak <zlatamakeeva19@gmail.com>
2024-10-03 21:25:18 +0300

    qwert

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -15,12 +15,8 @@ def main() -> None:
         print(profile)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        profile = create_language_profile('de', de_text)
-        print(profile)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-        profile = create_language_profile('unknown', unknown_text)
-        print(profile)
 
     result = profile
     assert result, "Detection result is None"

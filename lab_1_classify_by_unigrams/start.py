bec0dfbb1d9ad142ee95eb2ef6cb730e748e0148
ZlataMak <zlatamakeeva19@gmail.com>
2024-10-03 19:11:52 +0300

    qq

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,6 +2,7 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
+
 from lab_1_classify_by_unigrams.main import create_language_profile
 
 def main() -> None:

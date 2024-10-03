a113db885e527d5df199c8a118ad450d2b4c1823
Helenhelen2005 <elchevtaeva@gmail.com>
2024-10-03 19:15:50 +0300

    fixing something

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -3,9 +3,10 @@ Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
-from main import (collect_profiles, create_language_profile,
-                  detect_language, detect_language_advanced, load_profile,
-                  preprocess_profile, print_report, tokenize)
+from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
+                                             detect_language, detect_language_advanced,
+                                             load_profile, preprocess_profile, print_report,
+                                             tokenize)
 
 
 def main() -> None:

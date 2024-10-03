21bcfad973e89fb0a8d1810c324679d8970bba00
никнейм на гитхабе <LoraGellertova>
2024-10-03 22:59:14 +0300

    revert, add new file practice_1_intro_strings.py

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -105,6 +105,8 @@ def calculate_mse(predicted: list, actual: list) -> float | None:
         return None
     if len(predicted) != len(actual):
         return None
+    if len(predicted) == 0:
+        return None
 
     mse = 0
     for i in enumerate(predicted):

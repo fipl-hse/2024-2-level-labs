159c2a956501d637cc42f90cb5f8f468064bc307
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-10-04 00:46:45 +0300

    asdgfhg

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -204,7 +204,7 @@ def detect_language(
     if isinstance(mse1, float) and isinstance(mse2, float):
         if mse1 < mse2 and isinstance(profile_1['name'], str):
             return profile_1['name']
-        return profile_2['name']
+    return profile_2['name']
 
 
 def load_profile(path_to_file: str) -> dict | None:

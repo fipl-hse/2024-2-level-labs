9893e0143d4ee5d172470f4b6964f67f64a822a4
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-10-03 22:54:51 +0300

    asd

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -156,16 +156,16 @@ def compare_profiles(
     freq_list_to_compare = []
 
     for key in all_keys:
-        if not key in unknown_profile["freq"]:
+        if not key in unknown_profile["freq"] and and type(key) is str:
             unknown_freq_list.append(0.0)
         else:
             freq_of_key = unknown_profile["freq"][key]
-            unknown_freq_list.append(freq_of_key)
-        if not key in profile_to_compare["freq"]:
+            unknown_freq_list.append(float(freq_of_key))
+        if not key in profile_to_compare["freq"] and and type(key) is str:
             freq_list_to_compare.append(0.0)
         else:
             freq_of_key = profile_to_compare["freq"][key]
-            freq_list_to_compare.append(freq_of_key)
+            freq_list_to_compare.append(float(freq_of_key))
 
     return calculate_mse(unknown_freq_list, freq_list_to_compare)
 

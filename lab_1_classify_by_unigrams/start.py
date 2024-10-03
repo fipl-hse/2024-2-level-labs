d88575946248cb7eaf191b0f277a00061c52c065
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-10-04 00:27:53 +0300

    adfsf

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -15,10 +15,18 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    en_profile = create_language_profile('en', en_text)
-    de_profile = create_language_profile('de', de_text)
-    unknown_profile = create_language_profile('unknown', unknown_text)
-    result = detect_language(unknown_profile, en_profile, de_profile)
+    result = None
+
+    en_profile = create_language_profile("english", en_text)
+    de_profile = create_language_profile("german", de_text)
+    unknown_profile = create_language_profile("unknown", unknown_text)
+
+    if en_profile is not None and de_profile is not None and unknown_profile is not None:
+        result = detect_language(unknown_profile,
+                                 de_profile,
+                                 en_profile)
+    else:
+        result = None
     assert result, "Detection result is None"
 
 

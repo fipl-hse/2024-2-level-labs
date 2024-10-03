1e3404bb7fd6f88bfb0b1e76930baf029242db9e
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-10-03 20:42:40 +0300

    asda

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,8 @@
 Language detection starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable
-from main import create_language_profile, detect_language
+from lab_1_classify_by_unigrams.main import create_language_profile
+from lab_1_classify_by_unigrams.main import detect_language
 
 
 def main() -> None:

c12fa338f8a188eeae86093be9fe5078ae9105c2
minabdeyeva <olivia.aminel@yandex.ru>
2024-10-04 12:08:41 +0300

    last changes

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -4,7 +4,7 @@ Language detection starter
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 
 
-from main import tokenize, create_language_profile, detect_language
+from main import create_language_profile, detect_language
 
 
 def main() -> None:

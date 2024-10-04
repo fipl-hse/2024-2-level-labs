8eada3e479a9e216f1db9c61650422ad685ccac1
ZlataMak <zlatamakeeva19@gmail.com>
2024-10-04 11:57:32 +0300

    finaly

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -9,11 +9,15 @@ Language detection
 def tokenize(text: str) -> list[str] | None:
     """
     Split a text into tokens.
+
     Convert the tokens into lowercase, remove punctuation, digits and other symbols
+
     Args:
         text (str): A text
+
     Returns:
         list[str] | None: A list of lower-cased tokens without punctuation
+
     In case of corrupt input arguments, None is returned
     """
     if not isinstance(text, str):
@@ -28,10 +32,13 @@ def tokenize(text: str) -> list[str] | None:
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
     """
     Calculate frequencies of given tokens.
+
     Args:
         tokens (list[str] | None): A list of tokens
+
     Returns:
         dict[str, float] | None: A dictionary with frequencies
+
     In case of corrupt input arguments, None is returned
     """
     if not isinstance(tokens, list) or not all(isinstance(token, str) for token in tokens):
@@ -55,8 +62,10 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     Args:
         language (str): A language
         text (str): A text
+
     Returns:
         dict[str, str | dict[str, float]] | None: A dictionary with two keys – name, freq
+
     In case of corrupt input arguments, None is returned
     """
     if not isinstance(language, str) or not isinstance(text, str):

152fa269eef95e0e1646e6f709f432cd34c864a7
ZlataMak <zlatamakeeva19@gmail.com>
2024-10-04 11:53:42 +0300

    finall

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -7,6 +7,15 @@ Language detection
 
 
 def tokenize(text: str) -> list[str] | None:
+    """
+    Split a text into tokens.
+    Convert the tokens into lowercase, remove punctuation, digits and other symbols
+    Args:
+        text (str): A text
+    Returns:
+        list[str] | None: A list of lower-cased tokens without punctuation
+    In case of corrupt input arguments, None is returned
+    """
     if not isinstance(text, str):
         return None
     tokens = []
@@ -17,6 +26,14 @@ def tokenize(text: str) -> list[str] | None:
 
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
+    """
+    Calculate frequencies of given tokens.
+    Args:
+        tokens (list[str] | None): A list of tokens
+    Returns:
+        dict[str, float] | None: A dictionary with frequencies
+    In case of corrupt input arguments, None is returned
+    """
     if not isinstance(tokens, list) or not all(isinstance(token, str) for token in tokens):
         return None
     dict_freq = {}
@@ -32,7 +49,16 @@ def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
 
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
+    """
+    Create a language profile.
 
+    Args:
+        language (str): A language
+        text (str): A text
+    Returns:
+        dict[str, str | dict[str, float]] | None: A dictionary with two keys – name, freq
+    In case of corrupt input arguments, None is returned
+    """
     if not isinstance(language, str) or not isinstance(text, str):
         return None
     dict_freq = calculate_frequencies(tokenize(text))

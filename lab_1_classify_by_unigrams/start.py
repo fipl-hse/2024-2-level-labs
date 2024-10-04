5a6521b2581f0bf912b507cd5d1bdf8b638bca07
Anna Solovyova <solovy0va-aa@mail.ru>
2024-10-04 12:11:43 +0300

    mark6

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -11,16 +11,20 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        print(tokenize(en_text))
-        print(calculate_frequencies(en_text))
-    #with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
-    #    de_text = file_to_read_de.read()
-    #    print(tokenize(de_text))
-    #with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
-    #    unknown_text = file_to_read_unk.read()
-    #    print(tokenize(unknown_text))
-    result = None
-    assert result, "Detection result is None"
+        crop_text = tokenize(en_text)
+        print(calculate_frequencies(crop_text))
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
+        print(tokenize(de_text))
+        crop_text = tokenize(de_text)
+        print(calculate_frequencies(crop_text))
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+        unknown_text = file_to_read_unk.read()
+        print(tokenize(unknown_text))
+        crop_text = tokenize(unknown_text)
+        print(calculate_frequencies(crop_text))
+    #result = None
+    #assert result, "Detection result is None"
 
 
 if __name__ == "__main__":

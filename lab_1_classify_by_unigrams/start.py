d8887f899467a788281208550351bf5c7e59e039
artyomtugaryov <$artyomtugaryov@users.noreply.github.com>
2024-10-06 17:34:15 +0300

    "Update repository with upstream/main"

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -11,7 +11,6 @@ def main() -> None:
     """
     Launches an implementation
     """
-
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:

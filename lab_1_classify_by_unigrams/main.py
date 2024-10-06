e2c638e3870e3c1ef2529e82a6d0f48e694270e8
artyomtugaryov <$artyomtugaryov@users.noreply.github.com>
2024-10-06 17:32:48 +0300

    "Update repository with upstream/main"

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -1,16 +1,12 @@
 """
 Lab 1.
 
-
 Language detection
 """
 
 # pylint:disable=too-many-locals, unused-argument, unused-variable
 import copy
 import json
-
-import json
-from copy import deepcopy
 
 
 def tokenize(text: str) -> list[str] | None:

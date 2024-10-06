1dadc699c21fce89e642cdf4841bdd47cf7355e8
artyomtugaryov <$artyomtugaryov@users.noreply.github.com>
2024-10-06 17:34:08 +0300

    "Update repository with upstream/main"

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -8,6 +8,7 @@ Language detection
 import copy
 import json
 
+
 def tokenize(text: str) -> list[str] | None:
     """
     Split a text into tokens.
--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -7,20 +7,14 @@ from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_p
                                              print_report, tokenize)
 
 
-from lab_1_classify_by_unigrams.main import create_language_profile, tokenize
-
-
 def main() -> None:
     """
     Launches an implementation
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        # en_token = tokenize(en_text)
-        en_profile = create_language_profile("en", en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        de_profile = create_language_profile("de", de_text)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 

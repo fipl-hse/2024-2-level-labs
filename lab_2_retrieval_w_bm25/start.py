bd8ce8d3e36ef8c154fbc9f67e246e76fbe9d803
tasya-kakoulina <kaktotaksus@yandex.ru>
2024-10-07 20:45:30 +0300

    some mypy improvements

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -32,40 +32,59 @@ def main() -> None:
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
     tokenized_all_documents = []
-    avg = 0.0
     tf_idf_list = []
+    bm_list = []
+    bm_w_cutoff_list = []
+    avg = 0.0
     k1 = 1.5
     b = 0.75
     alpha = 0.2
-    bm_list = []
-    bm_w_cutoff_list = []
     query = 'Which fairy tale has Fairy Queen?'
     for document in documents:
         avg += len(document)
-        tokenized_all_documents.append(remove_stopwords(tokenize(document), stopwords))
+        tokenized_document = tokenize(document)
+        if tokenized_document is not None:
+            tokenized_all_documents.append(remove_stopwords(tokenized_document, stopwords))
     avg = avg / len(documents)
     vocab = build_vocabulary(tokenized_all_documents)
-    for document in documents:
-        length = len(document)
-        tokenization_res = remove_stopwords(tokenize(document), stopwords)
-        print(tokenization_res)
-        idf = calculate_idf(vocab, tokenized_all_documents)
-        tf_idf_list.append(calculate_tf_idf(calculate_tf(vocab, tokenization_res), idf))
-        bm_list.append(calculate_bm25(vocab, tokenization_res, idf, k1, b, avg, length))
-        bm_w_cutoff_list.append(calculate_bm25_with_cutoff(vocab, tokenization_res, idf, alpha, k1, b, avg, length))
+    if vocab is None:
+        return
+    idf = calculate_idf(vocab, tokenized_all_documents)
+    if idf is None:
+        return
+    for tokenized_document in tokenized_all_documents:
+        if tokenized_document is None:
+            return
+        length = len(tokenized_document)
+        print(tokenized_document)
+        tf_dict = calculate_tf(vocab, tokenized_document)
+        if tf_dict is not None:
+            tf_idf_for_doc = calculate_tf_idf(tf_dict, idf)
+            if tf_idf_for_doc is not None:
+                tf_idf_list.append(tf_idf_for_doc)
+        bm_for_doc = calculate_bm25(vocab, tokenized_document, idf, k1, b, avg, length)
+        if bm_for_doc is not None:
+            bm_list.append(bm_for_doc)
+        bm_w_cutoff_for_doc = calculate_bm25_with_cutoff(vocab, tokenized_document, idf, alpha, k1, b, avg, length)
+        if bm_w_cutoff_for_doc is not None:
+            bm_w_cutoff_list.append(bm_w_cutoff_for_doc)
     print(tf_idf_list)
     print(bm_list)
     tf_idf_rank_tuples = rank_documents(tf_idf_list, query, stopwords)
     bm_rank_tuples = rank_documents(bm_list, query, stopwords)
+    print(tf_idf_rank_tuples, bm_rank_tuples)
     bm_rank = [tup[0] for tup in bm_rank_tuples]
     tf_idf_rank = [tup[0] for tup in tf_idf_rank_tuples]
     save_index(bm_w_cutoff_list, 'assets/metrics.json')
     loaded_docs_list = load_index('assets/metrics.json')
+    if loaded_docs_list is None:
+        return
     bm_w_cutoff_rank_tuples = rank_documents(loaded_docs_list, query, stopwords)
     bm_w_cutoff_rank = [tup[0] for tup in bm_w_cutoff_rank_tuples]
-    print(calculate_spearman(tf_idf_rank, bm_w_cutoff_rank))
-    print(calculate_spearman(bm_rank, bm_w_cutoff_rank))
-    result = calculate_spearman(bm_rank, bm_w_cutoff_rank)
+    tf_result = calculate_spearman(tf_idf_rank, bm_w_cutoff_rank)
+    bm_result = calculate_spearman(bm_rank, bm_w_cutoff_rank)
+    print(tf_result, bm_result)
+    result = bm_result
     assert result, "Result is None"
 
 

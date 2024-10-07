20d570f9eb064aee81a0b9654b49fa076414ccbb
ibitsu <vi.alexina@gmail.com>
2024-10-07 21:04:19 +0300

    change start.py

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -2,6 +2,7 @@
 Laboratory Work #2 starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
+from lab_2_retrieval_w_bm25.main import tokenize
 
 
 def main() -> None:
@@ -26,7 +27,8 @@ def main() -> None:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = None
+    result = tokenize(documents[0])
+    print(result)
     assert result, "Result is None"
 
 

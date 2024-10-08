f942f745a717753e86aa3f46808ca8397f1a45d4
ibitsu <vi.alexina@gmail.com>
2024-10-08 20:45:32 +0300

    extend start.py

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -1,8 +1,9 @@
 """
 Laboratory Work #2 starter
 """
-# pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
-from lab_2_retrieval_w_bm25.main import tokenize
+from lab_2_retrieval_w_bm25.main import (tokenize, remove_stopwords, build_vocabulary,
+                                         calculate_tf, calculate_idf, calculate_tf_idf,
+                                         calculate_bm25, rank_documents)
 
 
 def main() -> None:
@@ -27,7 +28,40 @@ def main() -> None:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = tokenize(documents[0])
+
+    clear_documents = []
+    for document in documents:
+        tokenized_document = tokenize(document)
+        if not tokenized_document:
+            return None
+        clear_document = remove_stopwords(tokenized_document, stopwords)
+        if not clear_document:
+            return None
+        clear_documents.append(clear_document)
+
+    vocab = build_vocabulary(clear_documents)
+    if not vocab:
+        return None
+    idf = calculate_idf(vocab, clear_documents)
+    if not idf:
+        return None
+
+    tf_idf_list = []
+    bm25_list = []
+    for text in clear_documents:
+        tf = calculate_tf(vocab, text)
+        if not tf:
+            return None
+        len_text = len(text)
+        tf_idf_list.append(calculate_tf_idf(tf, idf))
+        bm25_list.append(calculate_bm25(vocab, text, idf, 1.5, 0.75,
+                                        sum(len(document) for document
+                                            in clear_documents) / len_text, len_text))
+
+    ranked_tf_idf = rank_documents(tf_idf_list, 'Which fairy tale has Fairy Queen?', stopwords)
+    ranked_bm25 = rank_documents(bm25_list, 'Which fairy tale has Fairy Queen?', stopwords)
+
+    result = (ranked_tf_idf, ranked_bm25)
     print(result)
     assert result, "Result is None"
 

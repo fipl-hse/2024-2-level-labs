f3a8f1de7d59ba2d622d123cf1f4195a0c3a298b
ibitsu <vi.alexina@gmail.com>
2024-10-09 14:54:39 +0300

    fix checks errors

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -1,11 +1,9 @@
 """
 Laboratory Work #2 starter
 """
-# pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
-from lab_2_retrieval_w_bm25.main import (tokenize, remove_stopwords, build_vocabulary,
-                                         calculate_tf, calculate_idf, calculate_tf_idf,
-                                         calculate_bm25, rank_documents, calculate_bm25_with_cutoff,
-                                         save_index, load_index, calculate_spearman)
+# pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches,
+# too-many-statements, duplicate-code
+import lab_2_retrieval_w_bm25.main as m
 
 
 def main() -> None:
@@ -35,17 +33,17 @@ def main() -> None:
     clear_documents = []
     for document in documents:
         avg_doc_len += len(document)
-        tokenized_document = tokenize(document)
+        tokenized_document = m.tokenize(document)
         if not tokenized_document:
             return None
-        clear_document = remove_stopwords(tokenized_document, stopwords)
+        clear_document = m.remove_stopwords(tokenized_document, stopwords)
         if not clear_document:
             return None
         clear_documents.append(clear_document)
-    vocab = build_vocabulary(clear_documents)
+    vocab = m.build_vocabulary(clear_documents)
     if not vocab:
         return None
-    idf_dict = calculate_idf(vocab, clear_documents)
+    idf_dict = m.calculate_idf(vocab, clear_documents)
     if not idf_dict:
         return None
     avg_doc_len /= len(clear_documents)
@@ -59,13 +57,13 @@ def main() -> None:
     for document in clear_documents:
         if not document:
             return None
-        tf_dict = calculate_tf(vocab, document)
+        tf_dict = m.calculate_tf(vocab, document)
         if not tf_dict:
             return None
         doc_len = len(document)
-        tf_idf = calculate_tf_idf(tf_dict, idf_dict)
-        bm25 = calculate_bm25(vocab, document, idf_dict, k1, b, avg_doc_len, doc_len)
-        optimized_bm25 = calculate_bm25_with_cutoff(vocab, document, idf_dict,
+        tf_idf = m.calculate_tf_idf(tf_dict, idf_dict)
+        bm25 = m.calculate_bm25(vocab, document, idf_dict, k1, b, avg_doc_len, doc_len)
+        optimized_bm25 = m.calculate_bm25_with_cutoff(vocab, document, idf_dict,
                                                     alpha, k1, b, avg_doc_len, doc_len)
         if not tf_idf or not bm25 or not optimized_bm25:
             return None
@@ -74,23 +72,23 @@ def main() -> None:
         bm25_plus_list.append(optimized_bm25)
 
     query = 'Which fairy tale has Fairy Queen?'
-    ranked_tf_idf = rank_documents(tf_idf_list, query, stopwords)
-    ranked_bm25 = rank_documents(bm25_list, query, stopwords)
-    ranked_optimized_bm25 = rank_documents(bm25_plus_list, query, stopwords)
+    ranked_tf_idf = m.rank_documents(tf_idf_list, query, stopwords)
+    ranked_bm25 = m.rank_documents(bm25_list, query, stopwords)
+    ranked_optimized_bm25 = m.rank_documents(bm25_plus_list, query, stopwords)
     if not ranked_tf_idf or not ranked_bm25 or not ranked_optimized_bm25:
         return None
 
-    save_index(bm25_plus_list, 'assets/metrics.json')
-    loaded_index = load_index('assets/metrics.json')
+    m.save_index(bm25_plus_list, 'assets/metrics.json')
+    loaded_index = m.load_index('assets/metrics.json')
     if not loaded_index:
         return None
-    ranked_index = rank_documents(loaded_index, query, stopwords)
+    ranked_index = m.rank_documents(loaded_index, query, stopwords)
     if not ranked_index:
         return None
 
     golden_rank = list(list(zip(*ranked_optimized_bm25))[0])
-    tf_idf_spearman = calculate_spearman(list(list(zip(*ranked_tf_idf))[0]), golden_rank)
-    bm25_spearman = calculate_spearman(list(list(zip(*ranked_bm25))[0]), golden_rank)
+    tf_idf_spearman = m.calculate_spearman(list(list(zip(*ranked_tf_idf))[0]), golden_rank)
+    bm25_spearman = m.calculate_spearman(list(list(zip(*ranked_bm25))[0]), golden_rank)
     if not tf_idf_spearman or not bm25_spearman:
         return None
 

ab6ecce83e7320c671d7648de0008c9baae0925e
ibitsu <vi.alexina@gmail.com>
2024-10-10 17:02:25 +0300

    fix checks errors

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -22,9 +22,11 @@ def main() -> None:
         "assets/fairytale_9.txt",
         "assets/fairytale_10.txt",
     ]
-    documents, clear_documents, tf_idf_list, bm25_list, optimized_bm25_list = [], [], [], [], []
     query, file_path = 'Which fairy tale has Fairy Queen?', 'assets/metrics.json'
     alpha, k1, b, avg_doc_len = 0.2, 1.5, 0.75, 0.0
+    tf_idf_list, bm25_list, optimized_bm25_list = [], [], []
+    documents, clear_documents, vocab = [], [], []
+    idf_dict, ranked_index = {}, []
 
     for path in paths_to_texts:
         with open(path, "r", encoding="utf-8") as file:
@@ -35,41 +37,34 @@ def main() -> None:
     for document in documents:
         avg_doc_len += len(document)
         tokenized_document = m.tokenize(document)
-        if not tokenized_document:
-            return None
-        clear_documents.append(m.remove_stopwords(tokenized_document, stopwords))
+        if tokenized_document:
+            clear_documents.append(m.remove_stopwords(tokenized_document, stopwords))
     avg_doc_len /= len(clear_documents)
 
     vocab = m.build_vocabulary(clear_documents)
-    if not vocab:
-        return None
-    idf_dict = m.calculate_idf(vocab, clear_documents)
-    if not idf_dict:
-        return None
+    if vocab:
+        idf_dict = m.calculate_idf(vocab, clear_documents)
 
     for document in clear_documents:
-        if not document:
-            return None
-        doc_len = len(document)
-        tf_dict = m.calculate_tf(vocab, document)
-        if tf_dict:
-            tf_idf = m.calculate_tf_idf(tf_dict, idf_dict)
-            if tf_idf:
-                tf_idf_list.append(tf_idf)
-        bm25 = m.calculate_bm25(vocab, document, idf_dict, k1, b, avg_doc_len, doc_len)
-        if bm25:
-            bm25_list.append(bm25)
-        optimized_bm25 = m.calculate_bm25_with_cutoff(vocab, document, idf_dict,
-                                                      alpha, k1, b, avg_doc_len, doc_len)
-        if optimized_bm25:
-            optimized_bm25_list.append(optimized_bm25)
+        if document:
+            doc_len = len(document)
+            tf_dict = m.calculate_tf(vocab, document)
+            if tf_dict:
+                tf_idf = m.calculate_tf_idf(tf_dict, idf_dict)
+                if tf_idf:
+                    tf_idf_list.append(tf_idf)
+            bm25 = m.calculate_bm25(vocab, document, idf_dict, k1, b, avg_doc_len, doc_len)
+            if bm25:
+                bm25_list.append(bm25)
+            optimized_bm25 = m.calculate_bm25_with_cutoff(vocab, document, idf_dict,
+                                                          alpha, k1, b, avg_doc_len, doc_len)
+            if optimized_bm25:
+                optimized_bm25_list.append(optimized_bm25)
 
     m.save_index(optimized_bm25_list, file_path)
     loaded_index = m.load_index(file_path)
-    if not loaded_index:
-        return None
-
-    ranked_index = m.rank_documents(loaded_index, query, stopwords)
+    if loaded_index:
+        ranked_index = m.rank_documents(loaded_index, query, stopwords)
     ranked_tf_idf = m.rank_documents(tf_idf_list, query, stopwords)
     ranked_bm25 = m.rank_documents(bm25_list, query, stopwords)
 

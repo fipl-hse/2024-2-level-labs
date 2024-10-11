a5684fd79149e37510864005fe5ca9f9b9578be7
evgraff2005 <annaevgrafova22095@gmail.com>
2024-10-11 12:21:31 +0300

    ...

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -2,8 +2,8 @@
 Laboratory Work #2 starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
-from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
-                                         calculate_tf_idf, remove_stopwords, tokenize)
+from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_bm25, calculate_idf,
+                                         calculate_tf, calculate_tf_idf, remove_stopwords, tokenize)
 
 
 def main() -> None:
@@ -41,11 +41,20 @@ def main() -> None:
                               remove_stopwords(tokenized_documents, stopwords))
             idf_dict = calculate_idf(build_vocabulary(tokenized_documents), tokenized_documents)
             tf_idf_dict = calculate_tf_idf(tf_dict, idf_dict)
+
+            '''avg_doc_len = len
+            for list_of_tokens in documents:
+                doc_len = len(list_of_tokens)
+                bm_25 = calculate_bm25(build_vocabulary(tokenized_documents),
+                                   tokenized_documents, idf_dict,
+                                   1.5, 0.75, avg_doc_len, doc_len)'''
+
     if (not isinstance(tf_idf_dict, dict) or not tf_idf_dict
             or not all(isinstance(tf_idf_dict_keys, str) for tf_idf_dict_keys in tf_idf_dict.keys()
             or not all(isinstance(tf_idf_dict_values, float)
                        for tf_idf_dict_values in tf_idf_dict.values()))):
-        return None
+        return
+
     result = tf_idf_dict
     assert result, "Result is None"
 

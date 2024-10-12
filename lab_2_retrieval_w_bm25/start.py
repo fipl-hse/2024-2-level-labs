7e2f8367a7aef6c0f3627562ecb58aef75b67d3f
semtassta <semtassta@gmail.com>
2024-10-12 14:22:29 +0300

    mark 6 start.py

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -1,6 +1,10 @@
 """
 Laboratory Work #2 starter
 """
+
+from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
+                                         calculate_tf_idf, remove_stopwords, tokenize)
+
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
 
 
@@ -26,7 +30,30 @@ def main() -> None:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = None
+
+    docs_tokenized = []
+    for document in documents:
+        doc_tokenized = tokenize(document)
+        if not isinstance(doc_tokenized, list):
+            return
+        doc_tokenized = remove_stopwords(doc_tokenized, stopwords)
+        if not isinstance(doc_tokenized, list):
+            return
+        docs_tokenized.append(doc_tokenized)
+    print(f'Demo 1: Removing Stop-Words\n{docs_tokenized}')
+
+    vocabulary = build_vocabulary(docs_tokenized)
+    if not isinstance(vocabulary, list):
+        return
+    print('Demo 2: Calculating TF-IDF')
+    for doc in docs_tokenized:
+        tf = calculate_tf(vocabulary, doc)
+        idf = calculate_idf(vocabulary, docs_tokenized)
+        if not isinstance(tf, dict) or not isinstance(idf, dict):
+            return
+        print(calculate_tf_idf(tf, idf))
+
+    result = 1
     assert result, "Result is None"
 
 

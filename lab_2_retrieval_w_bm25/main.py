51d81b7bd120897e283f29d23b09fffe3a7ed862
evgraff2005 <annaevgrafova22095@gmail.com>
2024-10-22 17:21:44 +0300

    ...

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -430,9 +430,9 @@ def load_index(file_path: str) -> list[dict[str, float]] | None:
     with open(file_path, 'r', encoding="utf-8") as file:
         load_index_file = json.load(file)
 
-    if load_index_file is None:
-        return None
-    return load_index_file
+    if load_index_file is not None and load_index_file is not any:
+        return load_index_file
+    return None
 
 
 def calculate_spearman(rank: list[int], golden_rank: list[int]) -> float | None:

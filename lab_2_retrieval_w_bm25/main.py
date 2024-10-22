89d483f4ad5633d73a26658791d5224ca36169fe
evgraff2005 <annaevgrafova22095@gmail.com>
2024-10-22 09:13:29 +0300

    ...

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -376,8 +376,9 @@ def calculate_bm25_with_cutoff(
         freq = document.count(word_from_vocab)
         if not isinstance(idf_document.get(word_from_vocab), float):
             return None
-        if idf_document.get(word_from_vocab) < alpha:
-            continue
+        if isinstance(alpha, float):
+            if idf_document.get(word_from_vocab) < alpha:
+                continue
         bm25_with_cutoff_figure = (idf_document[word_from_vocab]
                                    * ((freq*(k1+1)) / (freq+k1*(1-b+b*(doc_len/avg_doc_len)))))
         dict_bm25_with_cutoff[word_from_vocab] = bm25_with_cutoff_figure

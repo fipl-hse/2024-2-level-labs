62f25e8aab57fd9848e1d1e467fc8e9ce8835804
zakievamaria <zakievama@gmail.com>
2024-10-26 20:58:06 +0300

    and again

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -5,7 +5,6 @@ Laboratory Work #2 starter
 from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
                                          calculate_tf_idf, remove_stopwords, tokenize)
 
-
 def main() -> None:
     """
     Launches an implementation

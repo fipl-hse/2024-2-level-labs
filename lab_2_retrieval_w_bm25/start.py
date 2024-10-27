ec41717ec02ce90b5d9de6854180ba9a9fdbf1bd
MariaNikitenko19 <marianikitenko19@gmail.com>
2024-10-27 19:21:29 +0300

    fix

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -5,6 +5,7 @@ Laboratory Work #2 starter
 from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
                                          calculate_tf_idf, remove_stopwords, tokenize)
 
+
 def main() -> None:
     """
     Launches an implementation

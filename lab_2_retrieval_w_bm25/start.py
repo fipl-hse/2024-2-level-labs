ad3c3e3316b696700ce06fe3a7b311d01a5f1e00
kotelnikovalizo4ka <kotelnikovalizo4ka>
2024-10-28 23:10:25 +0300

    message

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -2,6 +2,8 @@
 Laboratory Work #2 starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
+from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
+                                         calculate_tf_idf, remove_stopwords, tokenize)
 
 
 def main() -> None:
@@ -21,13 +23,18 @@ def main() -> None:
         "assets/fairytale_10.txt",
     ]
     documents = []
+
     for path in paths_to_texts:
         with open(path, "r", encoding="utf-8") as file:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = None
-    assert result, "Result is None"
+    tokenized_documents = [remove_stopwords(tokenize(document), stopwords) for document in documents]
+    vocab = build_vocabulary(tokenized_documents)
+    tf_list = [calculate_tf(vocab, document) for document in tokenized_documents]
+    idf = calculate_idf(vocab, tokenized_documents)
+    tf_idf_list = [calculate_tf_idf(tf, idf) for tf in tf_list]
+    print(tf_idf_list)
 
 
 if __name__ == "__main__":

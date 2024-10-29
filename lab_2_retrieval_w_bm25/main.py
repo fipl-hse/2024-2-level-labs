0ca766791734ddc3e860e37504a0705597a84cfa
LexanDar11o6 <lexandar11o6@gmail.com@>
2024-10-29 23:25:08 +0300

    4

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -3,6 +3,8 @@ Lab 2.
 
 Text retrieval with BM25
 """
+
+
 # pylint:disable=too-many-arguments, unused-argument
 
 
@@ -19,6 +21,21 @@ def tokenize(text: str) -> list[str] | None:
     In case of corrupt input arguments, None is returned.
     """
 
+    if not isinstance(text, str):
+        return None
+
+    raw_text = []
+    for symbol in text:
+        if symbol.isalpha() or symbol == ' ':
+            raw_text.append(symbol)
+        if symbol == '\n':
+            raw_text.append('')
+
+    raw_text_str = ''.join(raw_text).lower()
+    tokens = raw_text_str.split(' ')
+
+    return tokens
+
 
 def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str] | None:
     """
@@ -33,6 +50,19 @@ def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str] | Non
 
     In case of corrupt input arguments, None is returned.
     """
+
+    document = []
+
+    if not isinstance(tokens, list) or not all(isinstance(value, str) for value in tokens):
+        return None
+    if not isinstance(stopwords, list) or not all(isinstance(value, str) for value in stopwords):
+        return None
+
+    for value in tokens:
+        if value not in stopwords:
+            document.append(value)
+
+    return document
 
 
 def build_vocabulary(documents: list[list[str]]) -> list[str] | None:
@@ -95,13 +125,13 @@ def calculate_tf_idf(tf: dict[str, float], idf: dict[str, float]) -> dict[str, f
 
 
 def calculate_bm25(
-    vocab: list[str],
-    document: list[str],
-    idf_document: dict[str, float],
-    k1: float = 1.5,
-    b: float = 0.75,
-    avg_doc_len: float | None = None,
-    doc_len: int | None = None,
+        vocab: list[str],
+        document: list[str],
+        idf_document: dict[str, float],
+        k1: float = 1.5,
+        b: float = 0.75,
+        avg_doc_len: float | None = None,
+        doc_len: int | None = None,
 ) -> dict[str, float] | None:
     """
     Calculate BM25 scores for a document.
@@ -123,7 +153,7 @@ def calculate_bm25(
 
 
 def rank_documents(
-    indexes: list[dict[str, float]], query: str, stopwords: list[str]
+        indexes: list[dict[str, float]], query: str, stopwords: list[str]
 ) -> list[tuple[int, float]] | None:
     """
     Rank documents for the given query.
@@ -141,14 +171,14 @@ def rank_documents(
 
 
 def calculate_bm25_with_cutoff(
-    vocab: list[str],
-    document: list[str],
-    idf_document: dict[str, float],
-    alpha: float,
-    k1: float = 1.5,
-    b: float = 0.75,
-    avg_doc_len: float | None = None,
-    doc_len: int | None = None,
+        vocab: list[str],
+        document: list[str],
+        idf_document: dict[str, float],
+        alpha: float,
+        k1: float = 1.5,
+        b: float = 0.75,
+        avg_doc_len: float | None = None,
+        doc_len: int | None = None,
 ) -> dict[str, float] | None:
     """
     Calculate BM25 scores for a document with IDF cutoff.
--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -2,6 +2,7 @@
 Laboratory Work #2 starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
+from lab_2_retrieval_w_bm25.main import (tokenize, remove_stopwords)
 
 
 def main() -> None:
@@ -26,7 +27,9 @@ def main() -> None:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = None
+    # print(tokenize(documents[0]))
+    # print(remove_stopwords(tokenize(documents[0]), stopwords))
+    result = remove_stopwords(tokenize(documents[0]),stopwords)
     assert result, "Result is None"
 
 

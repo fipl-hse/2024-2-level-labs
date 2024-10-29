3b6fbb686e1cdb3d24317330542c8bd397d3bc56
li-kuuu <liya2005@list.ru>
2024-10-30 00:25:05 +0300

    message

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -2,7 +2,8 @@
 Laboratory Work #2 starter
 """
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
-
+from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
+                                         calculate_tf_idf, remove_stopwords, tokenize)
 
 def main() -> None:
     """
@@ -27,8 +28,34 @@ def main() -> None:
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
     result = None
+    result = remove_stopwords
     assert result, "Result is None"
 
+    docs_tokens = []
+    for document in documents:
+        tokens = tokenize(document)
+        if tokens is not None:
+            cleared_tokens = remove_stopwords(tokens, stopwords)
+            docs_tokens.append(cleared_tokens)
+
+    vocab = build_vocabulary(docs_tokens)
+
+    idf = calculate_idf(vocab, docs_tokens) if vocab is not None else {}
+
+    tf_idf_list = []
+    for tokenized_doc in docs_tokens:
+        if isinstance(tokenized_doc, list) and vocab is not None:
+            tf = calculate_tf(vocab, tokenized_doc)
+            if tf is not None:
+                tf_idf = calculate_tf_idf(tf, idf)
+                if tf_idf is not None:
+                    tf_idf_list.append(tf_idf)
+
+    for index, tf_idf in enumerate(tf_idf_list):
+        result = tf_idf
+        print(f"TF-IDF for document {index + 1}:")
+        print(tf_idf)
+        assert result, "Result is None"
 
 if __name__ == "__main__":
     main()

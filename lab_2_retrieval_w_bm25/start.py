ea74975d728e2035717f29487e2b54b0f2a21805
minabdeyeva <olivia.aminel@yandex.ru>
2024-10-30 17:29:47 +0300

    start for 8

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -4,7 +4,8 @@ Laboratory Work #2 starter
 # pylint:disable=too-many-locals, unused-argument, unused-variable, too-many-branches, too-many-statements, duplicate-code
 
 from lab_2_retrieval_w_bm25.main import (build_vocabulary, calculate_idf, calculate_tf,
-                                         calculate_tf_idf, remove_stopwords, tokenize)
+                                         calculate_tf_idf, remove_stopwords, tokenize,
+                                         calculate_bm25, rank_documents)
 
 
 def main() -> None:
@@ -30,28 +31,107 @@ def main() -> None:
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
 
-
-    # for grade 6:
-
-    # metrics_dict = list[dict[str:bool]]
-    # metrics_dict = [{word1: TF-IDF1, word2: TF-IDF2...}, ...]
-    metrics = []
-
+    clean_docs = []
     for doc in documents:
-        tokenized_text = tokenize(doc)
-        clean_text = remove_stopwords(tokenized_text, stopwords)
-        vocab = build_vocabulary(clean_text)
-        tf_metric = calculate_tf(vocab, clean_text)
-        idf_metric = calculate_idf(vocab, clean_text)
-        tf_idf_metric = calculate_tf_idf(tf_metric, idf_metric)
-        metrics.append(tf_idf_metric)
+        doc_tokenized = tokenize(doc)
+        if doc_tokenized is None:
+            result = None
+            assert result, "Result is None"
+        clean_doc = remove_stopwords(doc_tokenized, stopwords)
+        if clean_doc is None:
+            result = None
+            assert result, "Result is None"
+        clean_docs.append(clean_doc)
+    if clean_docs is None:
+        result = None
+        assert result, "Result is None"
+    for doc in clean_docs:
+        if not isinstance(doc, list):
+            result = None
+            assert result, "Result is None"
+        for each in doc:
+            if not isinstance(each, str):
+                result = None
+                assert result, "Result is None"
 
+    vocabulary = build_vocabulary(clean_docs)
+    if not isinstance(vocabulary, list):
+        result = None
+        assert result, "Result is None"
+    for voc in vocabulary:
+        if not isinstance(voc, str):
+            result = None
+            assert result, "Result is None"
 
-    print(metrics)
+    tf_documents =  []
+    for doc in clean_docs:
+        if not isinstance(doc, list):
+            result = None
+            assert result, "Result is None"
+        for each in doc:
+            if not isinstance(each, str):
+                result = None
+                assert result, "Result is None"
+        tf_doc = calculate_tf(vocabulary, doc)
+        if any((tf_doc is None, not isinstance(tf_doc, dict))):
+            result = None
+            assert result, "Result is None"
+        for k in tf_doc:
+            if not isinstance(k, str):
+                result = None
+                assert result, "Result is None"
+            for each in tf_doc.values():
+                if not isinstance(each, float):
+                    result = None
+                    assert result, "Result is None"
+        tf_documents.append(tf_doc)
 
-    result = metrics
-    assert result, "Result is None"
+    # simplify
+    idf_documents = calculate_idf(vocabulary, clean_docs)
+    tf_idf_documents= []
+    for tf_doc in tf_documents:
+        if not isinstance(idf_documents, dict) \
+                or not all(isinstance(key, str) for key in idf_documents) \
+                or not all(isinstance(value, float) for value in idf_documents.values()):
+            result = None
+            assert result, "Result is None"
+        tf_idf = calculate_tf_idf(tf_doc, idf_documents)
+        if tf_idf is None:
+            result = None
+            assert result, "Result is None"
+        tf_idf_documents.append(tf_idf)
 
+    bm25_documents = []
+    avg_doc_len = sum(len(document) for document in clean_docs) / len(
+            clean_docs)
+    for doc_ in clean_docs:
+        if not isinstance(doc_, list) or not all(isinstance(item, str) for item in doc_):
+            result = None
+            assert result, "Result is None"
+        if idf_documents is None:
+            result = None
+            assert result, "Result is None"
+        bm25 = calculate_bm25(vocabulary, doc_, idf_documents,
+                              avg_doc_len=avg_doc_len, doc_len=len(doc_))
+        if bm25 is None:
+            result = None
+            assert result, "Result is None"
+        bm25_documents.append(bm25)
+
+    # simplify
+    query = 'Which fairy tale has Fairy Queen?'
+    tf_idf_ranked = rank_documents(tf_idf_documents, query, stopwords)
+    if tf_idf_ranked is None:
+        result = None
+        assert result, "Result is None"
+    bm25_ranked = rank_documents(bm25_documents, query, stopwords)
+    if bm25_ranked is None:
+        result = None
+        assert result, "Result is None"
+
+    result = bm25_ranked
+
+    assert result, 'Result in None'
 
 if __name__ == "__main__":
     main()

58a812540cfd6ffa9c7e1d2e9c7598dc726aa8df
SabirovaLilia <Lilia20s05@yandex.ru>
2024-10-31 23:21:47 +0300

    fix

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -319,7 +319,8 @@ def rank_documents(
             if ranged_documents[j][1] < ranged_documents[j + 1][1]:
                 (ranged_documents[j], ranged_documents[j + 1]) = (ranged_documents[j + 1],
                                                                   ranged_documents[j])
-
+    if not ranged_documents:
+        return None
     return ranged_documents
 
 

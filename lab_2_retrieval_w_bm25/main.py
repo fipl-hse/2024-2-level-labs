7afdae1d72fd8104a6416004b7d8a3057afaeec6
SabirovaLilia <Lilia20s05@yandex.ru>
2024-11-03 16:29:46 +0300

    revert

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -1,5 +1,6 @@
 """
 Lab 2.
+
 Text retrieval with BM25
 """
 # pylint:disable=too-many-arguments, unused-argument
@@ -10,10 +11,13 @@ from math import log
 def tokenize(text: str) -> list[str] | None:
     """
     Tokenize the input text into lowercase words without punctuation, digits and other symbols.
+
     Args:
         text (str): The input text to tokenize.
+
     Returns:
         list[str] | None: A list of words from the text.
+
     In case of corrupt input arguments, None is returned.
     """
     if not isinstance(text, str):
@@ -28,11 +32,14 @@ def tokenize(text: str) -> list[str] | None:
 def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str] | None:
     """
     Remove stopwords from the list of tokens.
+
     Args:
         tokens (list[str]): List of tokens.
         stopwords (list[str]): List of stopwords.
+
     Returns:
         list[str] | None: Tokens after removing stopwords.
+
     In case of corrupt input arguments, None is returned.
     """
     if not isinstance(tokens, list) or not all(isinstance(token, str) for token in tokens) or \
@@ -48,10 +55,13 @@ def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str] | Non
 def build_vocabulary(documents: list[list[str]]) -> list[str] | None:
     """
     Build a vocabulary from the documents.
+
     Args:
         documents (list[list[str]]): List of tokenized documents.
+
     Returns:
         list[str] | None: List with unique words from the documents.
+
     In case of corrupt input arguments, None is returned.
     """
     if not isinstance(documents, list) or \
@@ -71,11 +81,14 @@ def build_vocabulary(documents: list[list[str]]) -> list[str] | None:
 def calculate_tf(vocab: list[str], document_tokens: list[str]) -> dict[str, float] | None:
     """
     Calculate term frequency for the given tokens based on the vocabulary.
+
     Args:
         vocab (list[str]): Vocabulary list.
         document_tokens (list[str]): Tokenized document.
+
     Returns:
         dict[str, float] | None: Mapping from vocabulary terms to their term frequency.
+
     In case of corrupt input arguments, None is returned.
     """
     if not isinstance(vocab, list) or not all(isinstance(item, str) for item in vocab) or \
@@ -94,11 +107,14 @@ def calculate_tf(vocab: list[str], document_tokens: list[str]) -> dict[str, floa
 def calculate_idf(vocab: list[str], documents: list[list[str]]) -> dict[str, float] | None:
     """
     Calculate inverse document frequency for each term in the vocabulary.
+
     Args:
         vocab (list[str]): Vocabulary list.
         documents (list[list[str]]): List of tokenized documents.
+
     Returns:
         dict[str, float] | None: Mapping from vocabulary terms to its IDF scores.
+
     In case of corrupt input arguments, None is returned.
     """
     if not isinstance(vocab, list) or not all(isinstance(item, str) for item in vocab) or not vocab:
@@ -122,11 +138,14 @@ def calculate_idf(vocab: list[str], documents: list[list[str]]) -> dict[str, flo
 def calculate_tf_idf(tf: dict[str, float], idf: dict[str, float]) -> dict[str, float] | None:
     """
     Calculate TF-IDF scores for a document.
+
     Args:
         tf (dict[str, float]): Term frequencies for the document.
         idf (dict[str, float]): Inverse document frequencies.
+
     Returns:
         dict[str, float] | None: Mapping from terms to their TF-IDF scores.
+
     In case of corrupt input arguments, None is returned.
     """
     if not tf or not isinstance(tf, dict) or not all(isinstance(key, str) for key in tf) or \
@@ -153,6 +172,7 @@ def calculate_bm25(
 ) -> dict[str, float] | None:
     """
     Calculate BM25 scores for a document.
+
     Args:
         vocab (list[str]): Vocabulary list.
         document (list[str]): Tokenized document.
@@ -161,8 +181,10 @@ def calculate_bm25(
         b (float): BM25 parameter.
         avg_doc_len (float | None): Average document length.
         doc_len (int | None): Length of the document.
+
     Returns:
         dict[str, float] | None: Mapping from terms to their BM25 scores.
+
     In case of corrupt input arguments, None is returned.
     """
     if not vocab or not isinstance(vocab, list) or not all(isinstance(item, str) for item in vocab):
@@ -194,12 +216,15 @@ def rank_documents(
 ) -> list[tuple[int, float]] | None:
     """
     Rank documents for the given query.
+
     Args:
         indexes (list[dict[str, float]]): List of BM25 or TF-IDF indexes for the documents.
         query (str): The query string.
         stopwords (list[str]): List of stopwords.
+
     Returns:
         list[tuple[int, float]] | None: Tuples of document index and its score in the ranking.
+
     In case of corrupt input arguments, None is returned.
     """
     if not indexes or not isinstance(indexes, list) \
@@ -237,6 +262,7 @@ def calculate_bm25_with_cutoff(
 ) -> dict[str, float] | None:
     """
     Calculate BM25 scores for a document with IDF cutoff.
+
     Args:
         vocab (list[str]): Vocabulary list.
         document (list[str]): Tokenized document.
@@ -246,8 +272,10 @@ def calculate_bm25_with_cutoff(
         b (float): BM25 parameter.
         avg_doc_len (float | None): Average document length.
         doc_len (int | None): Length of the document.
+
     Returns:
         dict[str, float] | None: Mapping from terms to their BM25 scores with cutoff applied.
+
     In case of corrupt input arguments, None is returned.
     """
     if not vocab or not isinstance(vocab, list) or not all(isinstance(item, str) for item in vocab)\
@@ -276,6 +304,7 @@ def calculate_bm25_with_cutoff(
 def save_index(index: list[dict[str, float]], file_path: str) -> None:
     """
     Save the index to a file.
+
     Args:
         index (list[dict[str, float]]): The index to save.
         file_path (str): The path to the file where the index will be saved.
@@ -296,10 +325,13 @@ def save_index(index: list[dict[str, float]], file_path: str) -> None:
 def load_index(file_path: str) -> list[dict[str, float]] | None:
     """
     Load the index from a file.
+
     Args:
         file_path (str): The path to the file from which to load the index.
+
     Returns:
         list[dict[str, float]] | None: The loaded index.
+
     In case of corrupt input arguments, None is returned.
     """
     if not file_path or not isinstance(file_path, str):
@@ -313,11 +345,14 @@ def load_index(file_path: str) -> list[dict[str, float]] | None:
 def calculate_spearman(rank: list[int], golden_rank: list[int]) -> float | None:
     """
     Calculate Spearman's rank correlation coefficient between two rankings.
+
     Args:
         rank (list[int]): Ranked list of document indices.
         golden_rank (list[int]): Golden ranked list of document indices.
+
     Returns:
         float | None: Spearman's rank correlation coefficient.
+
     In case of corrupt input arguments, None is returned.
     """
     if not rank or not isinstance(rank, list) or not all(isinstance(item, int) for item in rank):

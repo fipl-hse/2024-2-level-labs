f9978b4bde3f43ceac9b2392439b92eff5dfb041
MariaManukhina <masamanukhina@gmail.com>
2024-11-07 22:24:26 +0300

    fix tokenize

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -22,7 +22,7 @@ def tokenize(text: str) -> list[str] | None:
     if not isinstance(text, str):
         return None
     tokenized_text_clear = []
-    for token in text.lower().:
+    for token in text.lower():
         if token.isalpha():
             tokenized_text_clear.append(token)
         else:

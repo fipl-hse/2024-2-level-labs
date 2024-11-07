f791406b739c5fbd311d372d28ae71a6a45281a4
MariaManukhina <masamanukhina@gmail.com>
2024-11-07 22:37:27 +0300

    fix tokenize

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -29,8 +29,6 @@ def tokenize(text: str) -> list[str] | None:
             text.replace(token," ")
             tokenized_text_clear.append(token)
     tokenized_text = " ".join(tokenized_text_clear).split()
-    if tokenized_text == []:
-        return None
     return tokenized_text
 def remove_stopwords(tokens: list[str], stopwords: list[str]) -> list[str] | None:
     """

de047a089b9c0a7b83ecc6d14ca78cb16000df47
никнейм на гитхабе <LoraGellertova>
2024-11-08 00:56:52 +0300

    revert, add new file practice_1_intro_strings.py

--- lab_2_retrieval_w_bm25/main.py
+++ lab_2_retrieval_w_bm25/main.py
@@ -21,7 +21,9 @@ def tokenize(text: str) -> list[str] | None:
     """
     if not isinstance(text, str):
         return None
-    punctuation = """!'"#$%&()*+,-./:;<=>?@[]^_`{|}~1234567890"""r"\""""
+    punctuation = ("!'\"#$%&()*+,-./:;<=>?@[]^_`{|}~1234567890"
+                   r'\"'
+                   "")
     for p in punctuation:
         if p in text:
             text = text.replace(p, ' ')
@@ -226,14 +228,9 @@ def calculate_bm25(
             or not isinstance(idf_document, dict) or \
             not isinstance(k1, float) or not isinstance(b, float):
         return None
-    if (
-            not isinstance(avg_doc_len, float) or
-            not isinstance(doc_len, int) or
-            not isinstance(doc_len, bool) or
-            len(vocab) == 0 or
-            len(document) == 0 or
-            len(idf_document) == 0
-    ):
+    if not isinstance(avg_doc_len, float) or not isinstance(doc_len, int) \
+            or isinstance(doc_len, bool) or \
+            len(vocab) == 0 or len(document) == 0 or len(idf_document) == 0:
         return None
     for word in vocab:
         if not isinstance(word, str):
@@ -277,7 +274,8 @@ def rank_documents(
         return None
     for index_dict in indexes:
         if (not isinstance(index_dict, dict) or
-                all(not isinstance(key, str) or not isinstance(values, float) for key, values in index_dict.items())):
+                all(not isinstance(key, str)
+                    or not isinstance(values, float) for key, values in index_dict.items())):
             return None
     for words in stopwords:
         if not isinstance(words, str):

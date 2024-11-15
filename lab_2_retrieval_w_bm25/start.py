3bf7905313031f9dd5b52f3b06eb026343480620
li-kuuu <liya2005@list.ru>
2024-11-15 09:46:42 +0300

    message

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -28,29 +28,24 @@ def main() -> None:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    result = None
-    result = remove_stopwords
-    assert result, "Result is None"
 
     tokenized_docs = []
     for doc in documents:
         tokenized_doc = tokenize(doc)
         tokenized_docs.append(tokenized_doc)
-    # print(tokenized_docs)
     meaningful_docs = []
     for doc in tokenized_docs:
         meaningful_doc = remove_stopwords(doc, stopwords)
         meaningful_docs.append(meaningful_doc)
-    # print(meaningful_docs)
     vocab = build_vocabulary(meaningful_docs)
-    # print (vocab)
     tf_values_lst = []
     for doc in meaningful_docs:
         tf_doc = calculate_tf(vocab, doc)
         tf_values_lst.append(tf_doc)
-    # print(tf_values_lst)
+    result = None
     for doc in tf_values_lst:
-        print(calculate_tf_idf(doc, calculate_idf(vocab, meaningful_docs)))
+        result = calculate_tf_idf(doc, calculate_idf(vocab, meaningful_docs))
+        print(result)
     assert result, "Result is None"
 
 

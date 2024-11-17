c14159b89c1df6cac88821e794ff6116627fa129
kotelnikovalizo4ka <kotelnikovalizo4ka>
2024-11-17 23:46:57 +0300

    message

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -4,6 +4,7 @@ Laboratory Work #3 starter.
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
 from pathlib import Path
+from main import Tokenizer
 
 
 def open_files() -> tuple[list[str], list[str]]:
@@ -21,7 +22,7 @@ def open_files() -> tuple[list[str], list[str]]:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    return (documents, stopwords)
+    return documents, stopwords
 
 
 def main() -> None:
@@ -30,7 +31,13 @@ def main() -> None:
     """
     with open("assets/secrets/secret_1.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
+    documents, stop_words = open_files()
+    documents.append(text)
+
+    tokenizer = Tokenizer(stop_words)
+    tokenized_documents = tokenizer.tokenize_documents(documents)
+
+    result = tokenized_documents
     assert result, "Result is None"
 
 

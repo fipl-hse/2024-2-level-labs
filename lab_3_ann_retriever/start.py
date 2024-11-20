16824ebadcb034cfbba4560b9b99b59a511b80af
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-11-21 01:26:21 +0300

    Start file function demonstration

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -3,10 +3,11 @@ Laboratory Work #3 starter.
 """
 
 from pathlib import Path
+from time import time
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
-from lab_3_ann_retriever.main import (BasicSearchEngine, NaiveKDTree, SearchEngine, Tokenizer,
-                                      Vectorizer)
+from lab_3_ann_retriever.main import (AdvancedSearchEngine, BasicSearchEngine, SearchEngine,
+                                      Tokenizer, Vectorizer)
 
 
 def open_files() -> tuple[list[str], list[str]]:
@@ -31,41 +32,52 @@ def main() -> None:
     """
     Launch an implementation.
     """
-    with open("assets/secrets/secret_1.txt", "r", encoding="utf-8") as text_file:
+    print(time())
+    with open("assets/secrets/secret_5.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    docs, stops = open_files()
+    text_elements = text.split(", ")
+    text_elements = tuple([float(element) for element in text_elements])
+    documents, stopwords = open_files()
 
-    tokenizer = Tokenizer(stops)
-    token_docs_list = tokenizer.tokenize_documents(docs)
+    tokenizer = Tokenizer(stopwords)
+    token_docs_list = tokenizer.tokenize_documents(documents)
 
     vectorizer = Vectorizer(token_docs_list)
     vectorizer.build()
-    vector = vectorizer.vectorize(token_docs_list[0])
-    tokens_from_vec = vectorizer.vector2tokens(vector)
+    secret_question = vectorizer.vector2tokens(text_elements)
+    vectorizer.save("assets/states/vectorizer_state.json")
+    vectorizer_again = Vectorizer(token_docs_list)
+    vectorizer_again.load("assets/states/vectorizer_state.json")
 
     searcher = BasicSearchEngine(vectorizer, tokenizer)
-    searcher.index_documents(docs)
+    searcher.index_documents(documents)
     relevant_docs = searcher.retrieve_relevant_documents("Нижний Новгород", 3)
-    vectorized_query = vectorizer.vectorize(tokenizer.tokenize("Нижний Новгород"))
-    closest_doc = searcher.retrieve_vectorized(vectorized_query)
-
-    naive_tree = NaiveKDTree()
-    naive_tree.build([(-0.26, -0.26), (0.0, -0.51), (-0.51, 0.0), (0.0, -0.26), (0.0, 0.0)
-        ])
-    print(naive_tree.query((-0.1, -0.2), 11))
+    print(time())
+    secret_answer = searcher.retrieve_vectorized(text_elements)
 
     better_searcher = SearchEngine(vectorizer, tokenizer)
-    better_searcher.index_documents(docs)
+    better_searcher.index_documents(documents)
     more_relevant_docs = better_searcher.retrieve_relevant_documents("Нижний Новгород")
+    print(time())
+    better_searcher.save("assets/states/engine_state.json")
 
-    result = tokens_from_vec
-    print(result, closest_doc, sep="\n")
+    best_searcher = AdvancedSearchEngine(vectorizer, tokenizer)
+    best_searcher.load("assets/states/engine_state.json")
+    most_relevant_docs = best_searcher.retrieve_relevant_documents("Нижний Новгород", 3)
+    print(time())
+
+    result = most_relevant_docs
+    print(secret_question, secret_answer, sep="\n")
     for doc in relevant_docs:
         print(doc)
     print()
-    for doc_tree in more_relevant_docs:
-        print(doc_tree)
+    for doc in more_relevant_docs:
+        print(doc)
+    print()
+    for doc in result:
+        print(doc)
     assert result, "Result is None"
+
 
 if __name__ == "__main__":
     main()

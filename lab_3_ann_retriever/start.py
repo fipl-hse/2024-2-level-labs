6dbafb61f6b6651bd3add21dbe36f1dbb8d33c37
kotelnikovalizo4ka <kotelnikovalizo4ka>
2024-11-20 15:45:34 +0300

    mesage

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -22,7 +22,7 @@ def open_files() -> tuple[list[str], list[str]]:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    return documents, stopwords
+    return (documents, stopwords)
 
 
 def main() -> None:
@@ -31,6 +31,7 @@ def main() -> None:
     """
     with open("assets/secrets/secret_1.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
+    result = None
     documents, stop_words = open_files()
     documents.append(text)
 

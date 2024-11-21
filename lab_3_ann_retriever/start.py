8f8cab619472a320013b51edf2f14bc8604c9fe1
Helenhelen2005 <elchevtaeva@gmail.com>
2024-11-21 10:24:25 +0300

    start.py upgrade for 8

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -5,7 +5,7 @@ Laboratory Work #3 starter.
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
 from pathlib import Path
 
-from lab_3_ann_retriever.main import BasicSearchEngine, Tokenizer, Vectorizer
+from lab_3_ann_retriever.main import BasicSearchEngine, Tokenizer, Vectorizer, SearchEngine
 
 
 def open_files() -> tuple[list[str], list[str]]:
@@ -40,13 +40,25 @@ def main() -> None:
     tokenized_docs = tokenizer.tokenize_documents(documents)
     if tokenized_docs is None:
         return
+    print('Tokenized documents:', tokenized_docs)
 
     vectorizer = Vectorizer(tokenized_docs)
     vectorizer.build()
 
     knn_retriever = BasicSearchEngine(vectorizer=vectorizer, tokenizer=tokenizer)
+    knn_retriever.index_documents(documents)
 
-    result = knn_retriever
+    vector = vectorizer.vectorize(tokenizer.tokenize(documents[0]))
+    print(vector)
+    token_vector = vectorizer.vector2tokens(vector)
+    print(token_vector)
+    knn_retriever.retrieve_vectorized(vector)
+
+    search_engine = SearchEngine(vectorizer, tokenizer)
+    search_engine.index_documents(documents)
+
+    query = 'Нижний Новгород'
+    result = search_engine.retrieve_relevant_documents(query)
     assert result, "Result is None"
 
 

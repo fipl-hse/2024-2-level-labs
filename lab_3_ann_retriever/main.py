135e31b863577c78f789f989a0ddfd66a0db2e37
Андрей Запекин <andrew@MacBook-Air-Andrej.local>
2024-11-21 23:53:39 +0300

    unittest

--- lab_3_ann_retriever/main.py
+++ lab_3_ann_retriever/main.py
@@ -314,10 +314,10 @@ class Vectorizer:
         for token, ind in self._token2ind.items():
             vector[ind] = text_tf[token] * self._idf_values[token]
 
-        if vector is None:
+        if vector is None or not isinstance(vector, tuple):
             return None
 
-        return tuple(vector)
+        return vector
 
 
 class BasicSearchEngine:

bd015965df613675e8bd17fc3fb9e87f7956dc69
Anna Solovyova <solovy0va-aa@mail.ru>
2024-11-21 20:14:30 +0300

    mark4

--- lab_3_ann_retriever/main.py
+++ lab_3_ann_retriever/main.py
@@ -91,6 +91,7 @@ class Tokenizer:
         Args:
             stop_words (list[str]): List with stop words
         """
+        self._stop_words = stop_words
 
     def tokenize(self, text: str) -> list[str] | None:
         """

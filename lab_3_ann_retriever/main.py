6a357111db58b231cad5c84cf9803b67396bc371
QuietHellsPage <baazhinga@yandex.ru>
2024-11-21 18:23:24 +0300

    greening

--- lab_3_ann_retriever/main.py
+++ lab_3_ann_retriever/main.py
@@ -4,9 +4,10 @@ Lab 3.
 Vector search with text retrieving
 """
 
+from math import sqrt
 # pylint: disable=too-few-public-methods, too-many-arguments, duplicate-code, unused-argument
 from typing import Protocol
-from math import sqrt
+
 from lab_2_retrieval_w_bm25.main import calculate_idf
 
 Vector = tuple[float, ...]

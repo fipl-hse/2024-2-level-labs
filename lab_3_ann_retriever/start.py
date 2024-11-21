e53e68c28602210ed91d69340ec08f126366c5ad
Helenhelen2005 <elchevtaeva@gmail.com>
2024-11-21 16:23:13 +0300

    fixing check start.py

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -64,7 +64,7 @@ def main() -> None:
     query = 'Нижний Новгород'
     most_relevant_doc = search_engine.retrieve_relevant_documents(query)
     if most_relevant_doc is None:
-        return None
+        return
     result = most_relevant_doc
     print('Most relevant document: ', result)
     assert result, "Result is None"

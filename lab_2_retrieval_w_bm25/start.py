cceb94e2a0adc651303131de5400e5f2760ae8d6
li-kuuu <liya2005@list.ru>
2024-11-22 10:28:09 +0300

    go

--- lab_2_retrieval_w_bm25/start.py
+++ lab_2_retrieval_w_bm25/start.py
@@ -32,23 +32,6 @@ def main() -> None:
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
 
-    tokenized_docs = []
-    for doc in documents:
-        tokenized_doc = tokenize(doc)
-        tokenized_docs.append(tokenized_doc)
-    meaningful_docs = []
-    for doc in tokenized_docs:
-        meaningful_doc = remove_stopwords(doc, stopwords)
-        meaningful_docs.append(meaningful_doc)
-    vocab = build_vocabulary(meaningful_docs)
-    tf_values_lst = []
-    for doc in meaningful_docs:
-        tf_doc = calculate_tf(vocab, doc)
-        tf_values_lst.append(tf_doc)
-    result = None
-    for doc in tf_values_lst:
-        result = calculate_tf_idf(doc, calculate_idf(vocab, meaningful_docs))
-        print(result)
     documents_preprocessed = []
     for document in documents:
         document_tokenized = tokenize(document)

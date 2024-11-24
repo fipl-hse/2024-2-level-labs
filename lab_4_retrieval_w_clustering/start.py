ec273219a93594b139bd1991a644962afaf6b154
Всезнающий лингвист <vsevolodpapin@gmail.com>
2024-11-24 21:50:21 +0300

    Import style check be like

--- lab_4_retrieval_w_clustering/start.py
+++ lab_4_retrieval_w_clustering/start.py
@@ -4,7 +4,13 @@ Laboratory Work #4 starter.
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
 
-from lab_4_retrieval_w_clustering.main import get_paragraphs, BM25Vectorizer, DocumentVectorDB, VectorDBSearchEngine
+from lab_4_retrieval_w_clustering.main import (
+    BM25Vectorizer,
+    DocumentVectorDB,
+    get_paragraphs,
+    VectorDBSearchEngine,
+)
+
 
 def open_files() -> tuple[list[str], list[str]]:
     """

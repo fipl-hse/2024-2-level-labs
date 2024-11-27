d2b51184d9e1af60f227887619ec85b6fe221a01
QuietHellsPage <baazhinga@yandex.ru>
2024-11-27 11:40:35 +0300

    greening

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -5,7 +5,6 @@ Laboratory Work #3 starter.
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
 from pathlib import Path
 
-
 from lab_3_ann_retriever.main import BasicSearchEngine, SearchEngine, Tokenizer, Vectorizer
 
 

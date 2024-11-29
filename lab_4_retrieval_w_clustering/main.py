74f7904d7d2b81c3e74e48b8fff885fde18825d6
ShrubberyBush <natablack58@gmail.com>
2024-11-29 18:54:08 +0300

    Fixes

--- lab_4_retrieval_w_clustering/main.py
+++ lab_4_retrieval_w_clustering/main.py
@@ -5,8 +5,9 @@ Vector search with clusterization
 """
 
 # pylint: disable=undefined-variable, too-few-public-methods, unused-argument, duplicate-code, unused-private-member, super-init-not-called
-from lab_3_ann_retriever.main import BasicSearchEngine, Tokenizer, Vector, Vectorizer
+
 from lab_2_retrieval_w_bm25.main import calculate_bm25
+from lab_3_ann_retriever.main import BasicSearchEngine, Tokenizer, Vector, Vectorizer
 
 Corpus = list[str]
 "Type alias for corpus of texts."

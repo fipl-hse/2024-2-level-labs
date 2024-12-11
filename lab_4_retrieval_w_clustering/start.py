ebad950894404a3d9957de444fd5554023f35862
awquari <marii.mishina@yandex.ru>
2024-12-11 11:08:26 +0300

    start done

--- lab_4_retrieval_w_clustering/start.py
+++ lab_4_retrieval_w_clustering/start.py
@@ -1,9 +1,16 @@
 """
 Laboratory Work #4 starter.
 """
+import json
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
-from lab_4_retrieval_w_clustering.main import DocumentVectorDB, VectorDBSearchEngine
+from lab_4_retrieval_w_clustering.main import (
+    ClusteringSearchEngine,
+    DocumentVectorDB,
+    VectorDBAdvancedSearchEngine,
+    VectorDBSearchEngine,
+    VectorDBTreeSearchEngine,
+)
 
 
 def open_files() -> tuple[list[str], list[str]]:
@@ -59,6 +66,22 @@ def main() -> None:
     result = search_engine.retrieve_relevant_documents(query, n_neighbours)
     for relevant_doc in result:
         print(relevant_doc)
+    n_neighbours = 5
+    k_means_search_engine = ClusteringSearchEngine(db)
+    result = k_means_search_engine.retrieve_relevant_documents(query, n_neighbours)
+    for relevant_doc in result:
+        print(relevant_doc)
+    k_means_search_engine.make_report(5, "assets/report.json")
+    with open("assets/report.json", "r", encoding="UTF-8") as f:
+        report = json.load(f)
+    print(report)
+    for cluster_q in range(1, 15):
+        clustering_search = ClusteringSearchEngine(db, n_clusters=cluster_q)
+        print(clustering_search.calculate_square_sum())
+    vector_tree_search = VectorDBTreeSearchEngine(db)
+    print(vector_tree_search.retrieve_relevant_documents(query, 1))
+    adv_vector_tree_search = VectorDBAdvancedSearchEngine(db)
+    print(adv_vector_tree_search.retrieve_relevant_documents(query, 5))
     assert result, "Result is None"
 
 

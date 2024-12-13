3f3d547fa66e5b74a9115c931190344eab0f12b0
Polina <nazhmutdinova678@mail.ru>
2024-12-13 10:37:06 +0300

    fix

--- lab_3_ann_retriever/start.py
+++ lab_3_ann_retriever/start.py
@@ -1,11 +1,8 @@
-"""
-Laboratory Work #3 starter.
-"""
+
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
 import time
 from pathlib import Path
-print("hello world")
 from lab_3_ann_retriever.main import (
     AdvancedSearchEngine,
     BasicSearchEngine,
@@ -30,7 +27,7 @@ def open_files() -> tuple[list[str], list[str]]:
             documents.append(file.read())
     with open("assets/stopwords.txt", "r", encoding="utf-8") as file:
         stopwords = file.read().split("\n")
-    return (documents, stopwords)
+    return documents, stopwords
 
 
 def main() -> None:

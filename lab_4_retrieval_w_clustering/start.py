52c4ced2df5561c0042f34e39a7acd6c2535cdd5
artyomtugaryov <$artyomtugaryov@users.noreply.github.com>
2024-12-16 21:05:14 +0300

    "Update repository with upstream/main"

--- lab_4_retrieval_w_clustering/start.py
+++ lab_4_retrieval_w_clustering/start.py
@@ -3,7 +3,6 @@ Laboratory Work #4 starter.
 """
 
 # pylint:disable=duplicate-code, too-many-locals, too-many-statements, unused-variable
-from lab_4_retrieval_w_clustering.main import DocumentVectorDB, get_paragraphs, VectorDBSearchEngine
 
 from lab_4_retrieval_w_clustering.main import (
     BM25Vectorizer,
